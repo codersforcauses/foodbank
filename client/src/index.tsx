34b7626b2ae13d8c5f4e25388fc12aa255e0f815
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-11-22 16:10:05 +0800

    restructure files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import './index.css'
+import reportWebVitals from 'Utils/reportWebVitals'
+import App from 'App'
+import 'index.css'
 
 ReactDOM.render(
   <React.StrictMode>
