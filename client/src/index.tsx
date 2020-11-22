a9a15281d6e2ac747987d62ec3d35b6b51226560
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-11-22 18:52:29 +0800

    remove css file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from 'Utils/reportWebVitals'
 import App from 'App'
-import 'index.css'
 
 ReactDOM.render(
   <React.StrictMode>
