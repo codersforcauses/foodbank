986fd10a2247e3c7d4470f3a91a39e92008ec205
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-11-23 01:09:04 +0800

    change file structure for pages

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Home from 'Home'
-import ErrorPage from 'Error'
+import Home from 'Pages/Home'
+import ErrorPage from 'Pages/Error'
 
 interface RouterElement {
   exact?: boolean
