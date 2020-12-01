ef93d90b7c3f3dbf4d52c5eab8d11e12d960c8f7
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-12-01 22:22:27 +0800

    add colours

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      primary: '#671e75',
+      blue: '#00acd0',
+      orange: '#df7400',
+      teal: '#47d5cd',
+      'dark-grey': '#2c2e35',
+      'light-grey': '#cecfcb',
+      white: '#ffffff',
+      black: '#000000'
+      // greenish-dark-grey: #83847A
+      // greenish-light-grey: #C6C6BC
+    },
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
