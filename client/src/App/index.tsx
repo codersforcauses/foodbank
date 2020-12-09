d4bc6b7ec67816312efd4549a7a4b34b1b4bde8f
neosh11 <neosh11@gmail.com>
2020-12-09 22:27:46 +0800

    Added music to the app

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,11 +1,15 @@
+import Audio from 'Components/Audio'
+
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import routes from 'router'
 
+const songLocation= 'https://www.w3schools.com/tags/horse.mp3';
 const App = () => {
   return (
     <div className='App'>
       <header>foodbank</header>
+      <Audio soundFile={songLocation} loop={true} />
       <Switch>
         {routes.map(({ name, ...route }) => (
           <Route key={name} {...route} />
