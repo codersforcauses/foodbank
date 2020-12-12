a756cdfd1599a3595c4a5da52e7b37da4ec73a89
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-12-13 04:35:42 +0800

    remove unecessary react import

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { FunctionComponent } from 'react'
 import Home from 'Pages/Home'
 import ErrorPage from 'Pages/Error'
 
@@ -6,7 +6,7 @@ interface RouterElement {
   exact?: boolean
   name: string
   path: string
-  component: React.FunctionComponent
+  component: FunctionComponent
   routes?: Array<RouterElement>
 }
 
