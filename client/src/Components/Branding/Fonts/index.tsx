dcc6225dfead27f00a3be27845d1e96469c6a461
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-12-16 14:06:11 +0800

    fix eslint

--- client/src/Components/Branding/Fonts/index.tsx
+++ client/src/Components/Branding/Fonts/index.tsx
@@ -4,7 +4,7 @@ export interface FontProps {
   /**
    * Font type
    */
-  type?: 'Abraham' | "Bliss"
+  type?: 'Abraham' | 'Bliss'
   /**
    * Font weight
    */
@@ -14,26 +14,34 @@ export interface FontProps {
    */
   size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
   both: boolean
-
 }
 
-const Fonts: React.FC<FontProps> = ({ type = 'Abraham', weight, size, both }) => both ?
-  <>
-    <p className={`font-serif font-${weight} text-${size}xl`}>
-      The quick brown fox jumps over the lazy dog.
-      <br /><br />
+const Fonts: React.FC<FontProps> = ({ type = 'Abraham', weight, size, both }) =>
+  both ? (
+    <>
+      <p className={`font-serif font-${weight} text-${size}xl`}>
+        The quick brown fox jumps over the lazy dog.
+        <br />
+        <br />
+        1234567890
+      </p>
+      <p className={`font-sans font-${weight} text-${size}xl`}>
+        The quick brown fox jumps over the lazy dog.
+        <br />
+        <br />
+        1234567890
+      </p>
+    </>
+  ) : (
+      <p
+        className={`${type === 'Abraham' ? 'font-serif' : 'font-sans'
+          } font-${weight} text-${size}xl`}
+      >
+        The quick brown fox jumps over the lazy dog.
+        <br />
+        <br />
       1234567890
-    </p>
-    <p className={`font-sans font-${weight} text-${size}xl`}>
-      The quick brown fox jumps over the lazy dog.
-      <br /><br />
-      1234567890
-    </p>
-  </> :
-  <p className={`${type === 'Abraham' ? 'font-serif' : 'font-sans'} font-${weight} text-${size}xl`}>
-    The quick brown fox jumps over the lazy dog.
-    <br /><br />
-    1234567890
-  </p>
+      </p>
+    )
 
 export default Fonts
