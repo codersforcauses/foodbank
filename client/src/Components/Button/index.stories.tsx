a4f324f53aaf9e7ffa58a901b76b4dd3460a5f88
Niall Gillmor <niall@gillmor.org>
2020-12-17 17:32:28 +0800

    Renamed button component files back to index

--- client/src/Components/Button/index.stories.tsx
+++ client/src/Components/Button/index.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
-import { Button, ButtonProps } from './Button'
+import { Button, ButtonProps } from './index'
 
 export default {
   title: 'Components/Button',
--- client/src/stories/Header.tsx
+++ client/src/stories/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Button } from 'Components/Button/Button'
+import { Button } from 'Components/Button/index'
 import './header.css'
 
 export interface HeaderProps {
