fdb00183c65d663872cb4a7e3ca32aa334305f64
Bronsen Ivulich <bronsenivulich.j@gmail.com>
2020-12-17 14:45:12 +0800

    fix button position & create classes

--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
+import './index.css'
 
 export const NavbarContent = () => {
-  return <nav>navbar here</nav>
+  return <nav className={`Navbar ${!open ? 'Stuck' : 'Button'}`}>
+      <ul id="list">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Resources</li>
+      </ul>
+    </nav>
 };
 
 export const Navbar: React.FC = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <header>
-      <button className="sm:hidden" onClick={() => setOpen(!open)}>Open Nav</button>
-      <div className={`sm:block ${!open ? 'hidden' : ''}`}>
-        <NavbarContent />
-      </div>
-    </header>
+      <header>
+        <span id="buttonSpan">
+          <button id="button" className="sm:hidden" onClick={() => setOpen(!open)}>+</button>
+          <div className={`sm:block ${!open ? 'hidden' : ''}`}>
+            < NavbarContent />
+          </div>
+       </span>
+     </header>
   );
 };
