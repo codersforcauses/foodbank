7c3db3c7b1fe0f8c24dc7050a1f2dcf06480de1d
Bronsen Ivulich <bronsenivulich.j@gmail.com>
2020-12-17 17:57:15 +0800

    style floating action button

--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 export const NavbarContent = () => {
   return (
     <nav>
-      <ul>
+      <ul id="list">
         <li>Home</li>
         <li>About Us</li>
         <li>Contact Us</li>
@@ -17,18 +17,31 @@ export const NavbarContent = () => {
 export const Navbar: React.FC = () => {
   const [open, setOpen] = useState(false)
 
+//my attempt at making a menu reveal animation, didn't work
+
+// function buttonAnimation() {
+//   let action = document.querySelector('.NavbarContent')!;
+//   action.classList.toggle('active')
+// }
+
+//wrapper for running two function onClick
+
+// function click() {
+//   {() => setOpen(!open)}
+//   {buttonAnimation()}
+// }
+
   return (
     <header>
       <span id='buttonSpan'>
-        <div className={`NavbarContent sm:block ${!open ? 'hidden' : ''}`}>
+        <div className={`NavbarContent sm:block ${!open ? 'hidden' : ''}`} >
           <NavbarContent />
         </div>
         <button
           className='sm:hidden FloatingButton'
           onClick={() => setOpen(!open)}
-          style={{}}
         >
-          +
+          <span>&#43;</span>
         </button>
       </span>
     </header>
