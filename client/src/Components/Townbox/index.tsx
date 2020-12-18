273d757c72f3cdb03b2435dd487c4afd6439f97c
Jonathan Choong <jonathanchoong4435@gmail.com>
2020-12-18 13:03:36 +0800

    added border to textbox and made textbox a fixed height

--- client/src/Components/Townbox/index.tsx
+++ client/src/Components/Townbox/index.tsx
@@ -38,20 +38,20 @@ export const Townbox: React.FC<TownboxProps> = ({
   return (
     <div className = "w-96 flex">
       
-      <h2 className = {['font-serif','text-white', 'p-2', 'border-black', 'border-2', 'rounded-md', 'ml-10', 'mt-6', 'absolute', 'z-10', headerColor].join(
+      <h2 className = {['font-serif','text-white', 'p-2', 'border-black', 'border-4', 'rounded-md', 'ml-10', 'mt-4', 'absolute', 'z-10', headerColor].join(
         ' '
       )}> {headerText} </h2>
       
-      <div className = "captionbox place-self-center p-2 mt-12 border-black flex relative flex-col items-center w-full">
+      <div className = "captionboxborder h-52 w-96 absolute mt-12"></div> 
 
-        <p className = "w-4/5 font-sans border-solid pb-2 mb-0 z-10 text-base relative mt-6 break-words">
+      <div className = "captionbox place-self-center p-2 mt-12 border-black flex relative flex-col items-center justify-center w-full h-52">
+        <p className = "w-4/5 font-sans border-solid pb-2 mb-0 z-10 text-base relative mt-4 break-words">
           {captionText}
         </p>
             
         {/* visit button is optional*/}
         {showButton && <Button label="visited" size="small" backgroundColor='#00acd0' primary={false}/>} 
-        </div>
-        
       </div>
+    </div>
   )
 }
