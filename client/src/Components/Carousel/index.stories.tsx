7ae70f92634f370274970e6d223552d46f3978d6
Eddie Atkinson <45678264+eddie-atkinson@users.noreply.github.com>
2020-12-28 15:06:06 +0800

    Adds fade transitions
    mostly working image transitions
    
    adds transition

--- client/src/Components/Carousel/index.stories.tsx
+++ client/src/Components/Carousel/index.stories.tsx
@@ -17,6 +17,20 @@ CarouselComponent.args = {
       src:
         'https://images.unsplash.com/photo-1608831260331-5ec49ca5d3ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
       alt: 'a photo of a theatre'
+    },
+    {
+      src:
+        'https://images.unsplash.com/photo-1608837354983-686b62cca140?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      alt: 'a photo of a river meeting a lake'
+    },
+    {
+      src:
+        'https://images.unsplash.com/photo-1609009835496-7121e5ade7d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80',
+      alt: 'some water'
     }
-  ]
+  ],
+  height: 600,
+  width: 800,
+  transition: 'fade',
+  duration: 1000
 }
--- client/src/Components/Carousel/index.tsx
+++ client/src/Components/Carousel/index.tsx
@@ -1,18 +1,101 @@
-import React from 'react'
-interface Image {
+/* eslint-disable */
+import React, { useState } from 'react'
+import { ArrowRight, ArrowLeft } from 'heroicons-react'
+import './index.css'
+type Image = {
   src: string
   alt: string
 }
 
 export interface CarouselProps {
+  /**
+   * Set of images with alt text to be rendered
+   */
   images: Array<Image>
+  /**
+   * Width in pixels
+   */
+  width?: number
+  /**
+   * Height in pixels
+   */
+  height?: number
+  /**
+   * Height in pixels
+   */
+  transition: 'slide' | 'fade' | ''
+  /**
+   * Duration in ms
+   */
+  duration?: number
 }
 
-const Carousel: React.FC<CarouselProps> = ({ images }) => {
-  const imagesArr = images.map(image => (
-    <img src={image.src} key={image.src} alt={image.alt} />
-  ))
-  return <div>{imagesArr}</div>
+const Carousel: React.FC<CarouselProps> = ({
+  images = [],
+  width = 800,
+  height = 600,
+  transition = '',
+  duration = 1000
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const imgsLen = images.length
+
+  const nextImage = (): void => {
+    setCurrentIndex((currentIndex + 1) % imgsLen)
+  }
+
+  const prevImage = (): void => {
+    setCurrentIndex(currentIndex === 0 ? imgsLen - 1 : currentIndex - 1)
+  }
+  const getDefaultItemClasses = (itemIndex: Number): string => {
+    const opacity = itemIndex === currentIndex ? 'opacity-100' : 'opacity-0'
+    const position =
+      itemIndex === currentIndex ? 'translate-x-0' : 'translate-x-full'
+    switch (transition) {
+      case 'fade': {
+        return `transition-opacity ease-out duration-${duration} ${opacity}`
+      }
+      case 'slide': {
+        return `transition-transform ease-out duration-${duration} transform  ${opacity} ${position}`
+      }
+      default: {
+        return `${opacity}`
+      }
+    }
+  }
+  const imagesArr = images.map((image, i) => {
+    return (
+      <>
+        <div
+          className={`carousel-item ${getDefaultItemClasses(
+            i
+          )} bg-cover rounded-lg`}
+          id={`images-${i}`}
+          style={{
+            backgroundImage: `url(${image.src})`
+          }}
+        ></div>
+      </>
+    )
+  })
+  return (
+    <div
+      className='relative shadow-lg rounded-lg'
+      style={{ width: width, height: height }}
+    >
+      <div className='z-10 relative h-full'>
+        <div className='flex items-center h-full justify-center justify-between'>
+          <div className='icon flex items-center justify-center rounded-lg text-white m-2 w-12 h-12'>
+            <ArrowLeft onClick={() => nextImage()} />
+          </div>
+          <div className='icon flex items-center justify-center rounded-lg text-white m-2 w-12 h-12'>
+            <ArrowRight onClick={() => prevImage()} />
+          </div>
+        </div>
+      </div>
+      {imagesArr}
+    </div>
+  )
 }
 
 export default Carousel
