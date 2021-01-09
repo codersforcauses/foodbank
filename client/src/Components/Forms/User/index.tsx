52336dddd8a2018febe45c9a9d5abddfb7c88c62
Bronsen Ivulich <bronsenivulich.j@gmail.com>
2021-01-09 15:02:13 +0800

    Add yup validation to login form

--- client/src/Components/Forms/User/index.tsx
+++ client/src/Components/Forms/User/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Form, Formik } from 'formik'
 import Input from 'Components/Input/TextField'
 import { Button } from 'Components/Button'
+import * as Yup from 'yup';
 
 interface UserForm {
   username: string
@@ -19,6 +20,21 @@ export interface UserProps {
   onSubmit: (values: UserForm) => void
 }
 
+const currentYear = new Date().getFullYear();
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(4, 'Username must be at least 4 characters')
+    .max(15, 'Username must be less than 15 characters')
+    .required('Required'),
+    // add validation to check if username is in db
+    year: Yup.number()
+    .max(currentYear, 'Enter a valid year')
+    .min(currentYear-110, 'Ok Boomer')
+    .typeError("Year must be a number")
+    .required('Required'),
+});
+
 export const User: React.FC<UserProps> = ({ title, onSubmit }) => {
   return (
     <>
@@ -26,6 +42,7 @@ export const User: React.FC<UserProps> = ({ title, onSubmit }) => {
         initialValues={{ username: '', year: 2010 }}
         initialErrors={{ username: 'Username Required', year: 'Year Required' }}
         onSubmit={onSubmit}
+        validationSchema={SignupSchema}
       >
         <Form>
           <div className='bg-white p-3 w-96 rounded-xl flex flex-col space-y-6'>
