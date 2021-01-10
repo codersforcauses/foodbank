3d1a1a6ff2cdc13a5e7155e0d487f56a53c119aa
Neosh <neosh11@icloud.com>
2021-01-10 14:17:36 +0800

    {reformat, updated dependency array} @ useAudio hook

--- client/src/Components/AudioComponent/index.tsx
+++ client/src/Components/AudioComponent/index.tsx
@@ -1,46 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-
-const useAudio: (url: string, loop: boolean)=> [boolean, ()=>void] = (url: string, loop: boolean): [boolean, () => void] => {
-    const [audio] = useState(new Audio(url));
-    const [playing, setPlaying] = useState(false);
-    audio.loop = loop;
-    const toggle = () => {
-        setPlaying(!playing);
+const useAudio: (url: string, loop: boolean) => [boolean, () => void] = (
+  url: string,
+  loop: boolean
+): [boolean, () => void] => {
+  const [audio] = useState(new Audio(url))
+  const [playing, setPlaying] = useState(false)
+  audio.loop = loop
+  const toggle = () => {
+    setPlaying(!playing)
+  }
+  useEffect(() => {
+    playing ? audio.play() : audio.pause()
+  }, [audio, playing])
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false))
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false))
     }
-    useEffect(() => {
-        playing ? audio.play() : audio.pause();
-    },
-        [playing]
-    );
-    useEffect(() => {
-        audio.addEventListener('ended', () => setPlaying(false));
-        return () => {
-            audio.removeEventListener('ended', () => setPlaying(false));
-        };
-    }, []);
-    return [playing, toggle];
-};
-
+  }, [audio])
+  return [playing, toggle]
+}
 
 interface Props {
-    soundFile: string;
-    loop: boolean;
+  soundFile: string
+  loop: boolean
 }
-const AudioComponent : React.FC<Props> = ({ soundFile, loop }: Props) => {
-
-    const [playing, toggle] = useAudio(soundFile, loop);
-    return (
-        <button className="bg-orange" style={
-            {
-                padding: 10,
-                top: 0,
-                right: 0,
-                position: "fixed",
-                zIndex: 3000
-            }
-        } onClick={toggle}>{playing ? "Pause" : "Play"} Music</button>
-    )
+const AudioComponent: React.FC<Props> = ({ soundFile, loop }: Props) => {
+  const [playing, toggle] = useAudio(soundFile, loop)
+  return (
+    <button
+      className='bg-orange'
+      style={{
+        padding: 10,
+        top: 0,
+        right: 0,
+        position: 'fixed',
+        zIndex: 3000
+      }}
+      onClick={toggle}
+    >
+      {playing ? 'Pause' : 'Play'} Music
+    </button>
+  )
 }
 
 export default AudioComponent
