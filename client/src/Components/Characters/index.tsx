0346e5507c7fe1d62d804b8c0d23d81ec77ae0a5
Burbuqe Shaqiri <burbuqe@Burbuqes-MacBook-Pro.local>
2021-01-16 14:40:20 +0800

    Transformation to another image

--- client/src/Components/Characters/index.tsx
+++ client/src/Components/Characters/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BlueBoy from './Dairy/BlueBoy.jpg'
 import YoBoy from './Dairy/YoBoy.jpg'
 
-const imgTransforms = [
+interface Character {
+  before: string
+  after: string
+  text: string
+}
+
+const imgTransforms: Array<Character> = [
   {
     before: BlueBoy,
     after: YoBoy,
@@ -10,15 +16,33 @@ const imgTransforms = [
   }
 ]
 
+const CharacterImg = ({ character }: { character: Character }) => {
+  const [image, setImage] = useState(character.before)
+  const changeImage = () => {
+    setImage(character.after)
+  }
+
+  return (
+    <span>
+      <img
+        src={image}
+        alt=''
+        width={500}
+        onClick={changeImage}
+        onKeyPress={changeImage}
+        role='presentation'
+      />
+      <p>{character.text}</p>
+    </span>
+  )
+}
+
 const Characters: React.FC = () => {
   return (
     <div>
       Characters:
-      {imgTransforms.map(img => (
-        <>
-          <img src={img.before} alt='' width={500} />
-          <p>{img.text}</p>
-        </>
+      {imgTransforms.map((img, index) => (
+        <CharacterImg key={index} character={img} />
       ))}
     </div>
   )
