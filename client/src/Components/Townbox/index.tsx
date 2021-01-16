dfdaed2b30d0875b319210f664a7427f20edd09d
jonathanchoong <jonathanchoong4435@gmail.com>
2021-01-16 16:03:43 +0800

    Auto stash before merge of "jonathan/townbox" and "origin/jonathan/townbox"

--- client/src/Components/Townbox/index.tsx
+++ client/src/Components/Townbox/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import './index.css'
-import {Button} from '../Button'
+import { Button } from '../Button'
 
 export interface TownboxProps {
   /**
@@ -32,25 +32,34 @@ export const Townbox: React.FC<TownboxProps> = ({
   headerText = 'headerText',
   captionText = 'captionText',
   headerColor = 'orange',
-  showButton = false,
-  widthSize = 550
+  showButton = false
 }) => {
   const bgColour = `bg-${headerColor}`
   return (
-    <div className = "flex" style={{width: widthSize}}>
-      
-      <h2 className = {`font-serif text-white p-2 px-4 border-black border-4 rounded-md ml-10 mt-4 absolute z-10 bg-${headerColor}`}
-      > {headerText} </h2>
-      
-      <div className = "captionboxborder h-64 absolute mt-12" style={{width: widthSize}}></div> 
-        
-      <div className = "captionbox place-self-center p-2 mt-12 border-black flex relative flex-col items-center justify-center w-full h-64">
-        <p className = "font-sans border-solid pb-2 mb-0 z-10 text-2xl relative mt-4 break-words leading-8" style={{width: widthSize - 100}}>
+    <div className='flex w-tb1 md:w-tb2 lg:w-tb3 xl:w-tb4'>
+      <h2
+        className={`font-serif text-white p-2 px-4 border-black border-4 rounded-md ml-10 mt-4 absolute z-10 text-2xl md:text-3xl ${bgColour}`}
+      >
+        {' '}
+        {headerText}{' '}
+      </h2>
+
+      <div className='captionboxborder h-64 absolute mt-12 w-tb1 md:w-tb2 lg:w-tb3 xl:w-tb4'></div>
+
+      <div className='captionbox place-self-center p-8 mt-12 border-black flex relative flex-col items-center justify-center w-full h-64 pb-2 pt-2'>
+        <p className='font-sans border-solid pb-2 mb-0 z-10 text-base leading-5 relative mt-4 break-words md:text-xl'>
           {captionText}
         </p>
-            
+
         {/* visit button is optional*/}
-        {showButton && <Button label="Visit" size="small" backgroundColor={headerColor==='orange' ? headerColor : '#671e75'} primary={false}/>} 
+        {showButton && (
+          <Button
+            label='Visit'
+            size='small'
+            backgroundColor={headerColor === 'orange' ? headerColor : 'primary'}
+            primary={false}
+          />
+        )}
       </div>
     </div>
   )
