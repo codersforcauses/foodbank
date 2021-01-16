f1caf0a49bcca9e10148d304cf3235607309dc43
Neosh <neosh11@icloud.com>
2021-01-16 14:57:15 +0800

    Revert "Removed hardcoded width"
    This reverts commit 99b01cfd5e0c35d7266149b8b91e5cf9c9607233.

--- client/src/Components/Townbox/index.tsx
+++ client/src/Components/Townbox/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import './index.css'
-import { Button } from '../Button'
+import {Button} from '../Button'
 
 export interface TownboxProps {
   /**
@@ -37,34 +37,24 @@ export const Townbox: React.FC<TownboxProps> = ({
   headerText = 'headerText',
   captionText = 'captionText',
   headerColor = 'orange',
-  showButton = false
+  showButton = false,
+  widthSize = 550
 }) => {
   return (
-    <div className='flex'>
-      <h2
-        className={`font-serif text-white p-2 px-4 border-black border-4 rounded-md ml-10 mt-4 absolute z-10 bg-${headerColor}`}
-      >
-        {' '}
-        {headerText}{' '}
-      </h2>
-      <div className='captionParent'>
-        <div className='captionBox p-2 mt-12 flex relative flex-col items-center justify-center w-64 sm:w-96 h-56 sm:h-64'>
-          <p className='captionText font-sans border-solid pb-2 mb-0 z-10 text-2xl relative mt-4 break-words leading-8'>
-            {captionText}
-          </p>
-
-          {/* visit button is optional*/}
-          {showButton && (
-            <Button
-              label='Visit'
-              size='small'
-              backgroundColor={
-                headerColor === 'orange' ? headerColor : '#671e75'
-              }
-              primary={false}
-            />
-          )}
-        </div>
+    <div className = "flex" style={{width: widthSize}}>
+      
+      <h2 className = {`font-serif text-white p-2 px-4 border-black border-4 rounded-md ml-10 mt-4 absolute z-10 bg-${headerColor}`}
+      > {headerText} </h2>
+      
+      <div className = "captionboxborder h-64 absolute mt-12" style={{width: widthSize}}></div> 
+        
+      <div className = "captionbox place-self-center p-2 mt-12 border-black flex relative flex-col items-center justify-center w-full h-64">
+        <p className = "font-sans border-solid pb-2 mb-0 z-10 text-2xl relative mt-4 break-words leading-8" style={{width: widthSize - 100}}>
+          {captionText}
+        </p>
+            
+        {/* visit button is optional*/}
+        {showButton && <Button label="Visit" size="small" backgroundColor={headerColor==='orange' ? headerColor : '#671e75'} primary={false}/>} 
       </div>
     </div>
   )
