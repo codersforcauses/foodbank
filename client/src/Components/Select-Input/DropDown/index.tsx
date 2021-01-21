17d466efd6749ee5f19f2cffcabdab0ced03af83
Jonathan Choong <jonathanchoong4435@gmail.com>
2021-01-21 17:38:11 +0800

    added mapping of array passed in as a prop

--- client/src/Components/Select-Input/DropDown/index.tsx
+++ client/src/Components/Select-Input/DropDown/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Listbox } from '@headlessui/react'
-export interface DropDownProps {}
+export interface DropDownProps {
+  //array of options
+  options?: Array<Text>
+  //disable select
+  // disabled?: boolean
+}
 
-export const DropDown: React.FC<DropDownProps> = ({}) => {
-  return <div></div>
+export const DropDown: React.FC<DropDownProps> = ({
+  options = ['1', '2', '3']
+  // disabled = false
+}) => {
+  const [selectedOption, setSelectedOption] = useState(options[0])
+  return (
+    <Listbox value={selectedOption} onChange={setSelectedOption}>
+      <Listbox.Label className='flex'>Select an Option: </Listbox.Label>
+      <Listbox.Button className='px-4 py-2 rounded bg-orange text-white'>
+        {
+          'select an option' //should be changed to something else later
+        }
+      </Listbox.Button>
+      <Listbox.Options className='items-center flex-col justify-content'>
+        {(options as any[]).map((option: string) => (
+          <Listbox.Option
+            key={option}
+            value={option}
+            className='text-center bg-white text-black my-0 -ml-8 border'
+          >
+            {option}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
+    </Listbox>
+  )
 }
