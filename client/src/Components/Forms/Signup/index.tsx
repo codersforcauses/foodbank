6accbe8982994c9fdcc9194e78d7856b979866fa
Benjamin Podmore <benjaminpodmore@gmail.com>
2021-01-23 21:57:05 +0800

    Create SignupForm story

--- client/src/Components/Forms/Signup/index.tsx
+++ client/src/Components/Forms/Signup/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Input from 'Components/Input/TextField'
 import Button from 'Components/Button'
 import { Form, Formik } from 'formik'
-import { Link } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
 
 import * as Yup from 'yup'
 
@@ -18,39 +18,36 @@ export interface SignupFormProps {
 const currentYear = new Date().getFullYear()
 
 const SignupSchema = Yup.object().shape({
-  username: Yup.string()
-    .trim()
-    .min(1, 'Username must be at least 1 character')
-    .required('Required'),
+  username: Yup.string().trim().required('Required'),
   year: Yup.number()
     .max(currentYear - 5, `Please enter a year before ${currentYear - 5}`)
     .typeError('Year must be a number')
     .required('Required')
 })
 
-export const SignupForm: React.FC<SignupFormProps> = ({
-  onSubmit,
-}) => {
+export const SignupForm: React.FC<SignupFormProps> = ({ onSubmit }) => {
   return (
-      <Formik
-        initialValues={{ username: '', year: 2000 }}
-        onSubmit={onSubmit}
-        validationSchema={SignupSchema}
-      >
-        <Form>
-          <div className='bg-white p-3 sm:w-96 rounded-xl flex flex-col space-y-6'>
-            <h1 className='text-3xl'>Login</h1>
-            <Input label='Username' name='username' />
-            <Input label='Year of Birth' name='year' />
-            <Button type='submit'>Submit</Button>
-            <div className='pageStatus flex justify-center'>
+    <Formik
+      initialValues={{ username: '', year: 2000 }}
+      onSubmit={onSubmit}
+      validationSchema={SignupSchema}
+    >
+      <Form>
+        <div className='bg-white p-3 sm:w-96 rounded-xl flex flex-col space-y-6'>
+          <h1 className='text-3xl'>Sign Up</h1>
+          <Input label='Username' name='username' />
+          <Input label='Year of Birth' name='year' />
+          <Button type='submit'>Submit</Button>
+          <div className='flex justify-center'>
+            <BrowserRouter>
               <Link to='/login' className='ml-2'>
-                Don&apos;t have an account?
+                Already have an account?
               </Link>
-            </div>
+            </BrowserRouter>
           </div>
-        </Form>
-      </Formik>
+        </div>
+      </Form>
+    </Formik>
   )
 }
 
