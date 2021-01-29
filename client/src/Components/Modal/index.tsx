0b50ae614fa0e0f324ab4d78c2338ba995cdaf71
Niall Gillmor <niall@gillmor.org>
2021-01-29 13:37:54 +0800

    Added focus trap for keyboard accessbility

--- client/src/Components/Modal/index.tsx
+++ client/src/Components/Modal/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions*/
 /* eslint-disable jsx-a11y/click-events-have-key-events*/
 
+import FocusTrap from 'focus-trap-react'
 import React, { useCallback, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
@@ -38,35 +39,37 @@ const ModalContainer: React.FC<ModalContainerProps> = ({
     const portalDiv = document.getElementById('portal')
     return portalDiv
       ? ReactDOM.createPortal(
-          <div
-            className='flex flex-wrap justify-center content-center bg-black bg-opacity-50 z-50 absolute inset-0'
-            onClick={onClose}
-            role='dialog'
-          >
+          <FocusTrap>
             <div
-              className='max-w-md min-w-320 bg-primary p-5 rounded-xl flex flex-col shadow-2xl'
-              onClick={event => event.stopPropagation()}
+              className='flex flex-wrap justify-center content-center bg-black bg-opacity-50 z-50 absolute inset-0'
+              onClick={onClose}
               role='dialog'
             >
-              <div id='modal-header' className='grid grid-cols-3 pb-5'>
-                <h2 className='text-3xl col-start-2 font-serif uppercase justify-self-center text-white'>
-                  {header}
-                </h2>
-                <button
-                  className='justify-self-end font-serif text-red text-xl shadow-2xl'
-                  onClick={onClose}
-                >
-                  X
-                </button>
-              </div>
               <div
-                id='modal-body'
-                className='font-sans bg-white rounded-3xl p-4'
+                className='max-w-md min-w-320 bg-primary p-5 rounded-xl flex flex-col shadow-2xl'
+                onClick={event => event.stopPropagation()}
+                role='dialog'
               >
-                {children}
+                <div id='modal-header' className='grid grid-cols-3 pb-5'>
+                  <h2 className='text-3xl col-start-2 font-serif uppercase justify-self-center text-white'>
+                    {header}
+                  </h2>
+                  <button
+                    className='justify-self-end font-serif text-red text-xl shadow-2xl'
+                    onClick={onClose}
+                  >
+                    X
+                  </button>
+                </div>
+                <div
+                  id='modal-body'
+                  className='font-sans bg-white rounded-3xl p-4'
+                >
+                  {children}
+                </div>
               </div>
             </div>
-          </div>,
+          </FocusTrap>,
           portalDiv
         )
       : null
