1ede77727439bbece1630d86191ac37eadf53a0f
thaingu <thain7127@gmail.com>
2021-01-30 17:24:08 +0800

    rename nav to header

--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -7,12 +7,12 @@ export interface NavbarProps {
 
 export const Navbar: React.FC<NavbarProps> = ({ links }) => {
   return (
-    <nav className='bg-primary'>
+    <header className='bg-primary'>
       <ul className='flex flex-col lg:flex-row justify-center'>
         {links.map(nav => (
           <Navlink key={nav.page} {...nav} />
         ))}
       </ul>
-    </nav>
+    </header>
   )
 }
