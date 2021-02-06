7e81f6fe8444bfef0bc9e3e90122ee500f87b9f1
Benjamin Podmore <46699356+benjaminpodmore@users.noreply.github.com>
2021-02-06 14:45:26 +0800

    Switch import statements

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import routes from 'router'
 
 import { AuthProvider } from 'Contexts/AuthContext'
