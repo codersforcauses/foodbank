b46a72c9990b7ea36780d89945000bf4dad1a968
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-02-20 15:39:52 +0800

    fixed margins and props for all steps page

--- client/src/Components/RecipeStep/index.tsx
+++ client/src/Components/RecipeStep/index.tsx
@@ -4,23 +4,20 @@ import { ColorScheme, RecipeStep } from 'lib/types'
 interface TabProps {
   step: RecipeStep
   colorScheme: ColorScheme
-  [x: string]: any
+  key: number
 }
 
 const RecipeStepTab: React.FC<TabProps> = ({
   step,
   colorScheme,
-  ...props
+  key
 }: TabProps) => (
-  <div
-    className='flex flex-row items-center my-5 w-screen space-x-8'
-    {...props}
-  >
-    <h1 className={'text-4xl font-serif ' + colorScheme.header}>
+  <div className='flex flex-row items-center my-5 w-screen space-x-8' key={key}>
+    <h1 className={'w-12 text-4xl font-serif ' + colorScheme.header}>
       {step.number}
     </h1>
-    <img className='w-80 rounded' src={step.image} alt='Step' />
-    <p className={colorScheme.text}>{step.description}</p>
+    <img className='w-80 rounded-3xl' src={step.image} alt='Step' />
+    <p className={'w-1/2 ' + colorScheme.text}>{step.description}</p>
   </div>
 )
 
