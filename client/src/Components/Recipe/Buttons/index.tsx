4a02804524899843f0703a92e6d3aaea869babca
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-27 15:00:24 +0800

    refactored recipe buttons to separate component

---
+++ client/src/Components/Recipe/Buttons/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Recipe } from 'lib/types'
+
+interface Props {
+  recipe: Recipe
+}
+
+const Buttons: React.FC<Props> = ({ recipe }) => {
+  const colorScheme = recipe.colorScheme
+  return (
+    <div className='flex flex-row space-x-2 items-center'>
+      <Link to={'/recipe/' + recipe.slug + '/all-steps'}>
+        <button
+          className={
+            'w-48 my-4 py-2 px-4 rounded-full ' +
+            colorScheme.buttonBg +
+            ' ' +
+            colorScheme.buttonText
+          }
+        >
+          View All Steps
+        </button>
+      </Link>
+      <Link to={'/recipe/' + recipe.slug + '/slideshow'}>
+        <button
+          className={
+            'w-48 my-4 py-2 px-4 rounded-full ' +
+            colorScheme.buttonBg +
+            ' ' +
+            colorScheme.buttonText
+          }
+        >
+          Let&apos;s Cook
+        </button>
+      </Link>
+    </div>
+  )
+}
+
+export default Buttons
--- client/src/Components/RecipeOverviewTab/index.tsx
+++ client/src/Components/RecipeOverviewTab/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Recipe } from 'lib/types'
 
+import Buttons from 'Components/Recipe/Buttons'
 import EquipmentList from 'Components/Recipe/EquipmentList'
 import IngredientsList from 'Components/Recipe/IngredientsList'
 import CategoryInfo from 'Components/Recipe/CategoryInfo'
@@ -10,45 +10,8 @@ interface Props {
   recipe: Recipe
 }
 
-interface ButtonsProps {
-  recipe: Recipe
-}
-
-const Buttons: React.FC<ButtonsProps> = ({ recipe }: ButtonsProps) => {
-  const colorScheme = recipe.colorScheme
-  return (
-    <div className='flex flex-row space-x-2 items-center'>
-      <Link to={'/recipe/' + recipe.slug + '/all-steps'}>
-        <button
-          className={
-            'w-48 my-4 py-2 px-4 rounded-full ' +
-            colorScheme.buttonBg +
-            ' ' +
-            colorScheme.buttonText
-          }
-        >
-          View All Steps
-        </button>
-      </Link>
-      <Link to={'/recipe/' + recipe.slug + '/slideshow'}>
-        <button
-          className={
-            'w-48 my-4 py-2 px-4 rounded-full ' +
-            colorScheme.buttonBg +
-            ' ' +
-            colorScheme.buttonText
-          }
-        >
-          Let&apos;s Cook
-        </button>
-      </Link>
-    </div>
-  )
-}
-
 const RecipeOverview: React.FC<Props> = ({ recipe }: Props) => {
   const colorScheme = recipe.colorScheme
-
   return (
     <div
       className={
