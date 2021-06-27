5c24c87d6067e0d930a5d33f22c25595e1b0d350
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-06-27 14:55:43 +0800

    refactored category info to separate component

--- client/src/Components/RecipeOverviewTab/index.tsx
+++ client/src/Components/RecipeOverviewTab/index.tsx
@@ -4,6 +4,7 @@ import { Recipe } from 'lib/types'
 
 import EquipmentList from 'Components/Recipe/EquipmentList'
 import IngredientsList from 'Components/Recipe/IngredientsList'
+import CategoryInfo from 'Components/Recipe/CategoryInfo'
 
 interface Props {
   recipe: Recipe
@@ -62,19 +63,7 @@ const RecipeOverview: React.FC<Props> = ({ recipe }: Props) => {
         >
           {recipe.name}
         </h1>
-        <h2 className={'text-xl font-serif ' + colorScheme.header}>Category</h2>
-        <p className={colorScheme.text}>{recipe.category}</p>
-        <h2 className={'text-xl font-serif ' + colorScheme.header}>Tags</h2>
-        <div className='flex flex-row flex-wrap'>
-          {recipe.tags.map(tag => (
-            <div
-              key={tag}
-              className='mr-3 my-0.5 px-4 rounded-3xl bg-light-grey text-sm'
-            >
-              {tag}
-            </div>
-          ))}
-        </div>
+        <CategoryInfo recipe={recipe} />
         <IngredientsList recipe={recipe} />
         <EquipmentList recipe={recipe} />
         <Buttons recipe={recipe} />
