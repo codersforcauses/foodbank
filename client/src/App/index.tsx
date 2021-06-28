05c94274288a5b5c6a906ed10acf6fa651c19b59
skyheat <skyheatcp@gmail.com>
2021-06-28 17:18:30 +0800

    Added link to map in navbar

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import routes from 'router'
+import { Navbar } from 'Components/Navbar'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <header>foodbank</header>
+      <Navbar></Navbar>
       <Switch>
         {routes.map(({ name, ...route }) => (
           <Route key={name} {...route} />
--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const NavbarContent = () => {
-  return <nav>navbar here</nav>
+  return (
+    <>
+    <nav>navbar here</nav>
+    <nav>
+      <Link to="/">Map</Link>
+    </nav>
+    </>
+    )
 };
 
 export const Navbar: React.FC = () => {
