1d98659e2b4168272c48f6a506741be13364c1a2
Nicholas Choong <nicholascwc1997@gmail.com>
2021-06-29 19:21:51 +0800

    Replaced switch navbar with functional navbar

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+// import { Switch, Route } from 'react-router-dom'
 import routes from 'router'
 import { Navbar } from 'Components/Navbar'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
-      <header>foodbank</header>
-      <Navbar></Navbar>
-      <Switch>
-        {routes.map(({ name, ...route }) => (
-          <Route key={name} {...route} />
-        ))}
-      </Switch>
+      <header>
+        foodbank
+        <Navbar links={routes} />
+      </header>
     </div>
   )
 }
 
+// <Switch>
+//   {routes.map(({ name, ...route }) => (
+//     <Route key={name} {...route} />
+//   ))}
+// </Switch>
 export default App
