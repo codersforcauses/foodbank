3255fbe38bb3acb0f991028ffe1f95e12f2d5b2c
Nicholas Choong <nicholascwc1997@gmail.com>
2021-06-29 20:47:14 +0800

    Replace a tag with Link tag

--- client/src/Components/Navbar/Navlink/index.tsx
+++ client/src/Components/Navbar/Navlink/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { RouterElement } from '../../../router'
-import { Route } from 'react-router-dom'
 
 // export interface NavlinkProps {
 //   page: string
 //   route: string
 // }
 
-export const Navlink: React.FC<RouterElement> = ({ name, path, component }) => {
-  return <Route exact path={path} clasName={name} component={component} />
+export const Navlink: React.FC<RouterElement> = ({ name, path }) => {
+  return (
+    <li className={name.toLowerCase()}>
+      <Link to={path}>{name}</Link>
+    </li>
+  )
 }
-
-// <a className='text-white font-serif p-2 hover:opacity-75' href={route}>
-//   {page}
-// </a>
