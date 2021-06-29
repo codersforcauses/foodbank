71583aa3e5604a3b4d75cfb18e2c8baa6706a065
Nicholas Choong <nicholascwc1997@gmail.com>
2021-06-29 20:43:31 +0800

    Added routing component for the links in navbar

--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -1,40 +1,43 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-
-// export const NavbarContent = () => {
-//   return (
-//     <>
-//     <nav>navbar here</nav>
-//     <nav>
-//       <Link to="/">Map</Link>
-//     </nav>
-//     </>
-//     )
-// };
-
-// export const Navbar: React.FC = () => {
-//   const [open, setOpen] = useState(false);
 import React from 'react'
 import { Navlink } from './Navlink'
-import { RouterElement } from '../../router'
-import { BrowserRouter as Router } from 'react-router-dom'
+import routes, { RouterElement } from '../../router'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 export interface NavbarProps {
   links: RouterElement[]
 }
 
+const Routing: React.FC = () => {
+  return (
+    <div className='w-100'>
+      <Switch>
+        {routes.map(nav => (
+          <Route
+            exact
+            path={nav.path}
+            key={nav.name}
+            component={nav.component}
+          />
+        ))}
+      </Switch>
+    </div>
+  )
+}
+
 export const Navbar: React.FC<NavbarProps> = ({ links }) => {
   return (
-    <header className='bg-primary'>
-      <nav className='navbar'>
-        <Router>
-          <ul className='flex flex-col lg:flex-row justify-center'>
+    <Router>
+      <header className='bg-primary'>
+        foodbank
+        <nav className='navbar'>
+          <ul className=''>
             {links.map(nav => (
               <Navlink key={nav.name} {...nav} />
             ))}
           </ul>
-        </Router>
-      </nav>
-    </header>
+        </nav>
+      </header>
+      <Routing />
+    </Router>
   )
 }
