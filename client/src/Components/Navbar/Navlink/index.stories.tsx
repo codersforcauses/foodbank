a9c8a3da0e8924fd6803a2c3577b042defb3402c
Nicholas Choong <nicholascwc1997@gmail.com>
2021-06-29 19:23:42 +0800

    Changed type from NavlinkProps to RouterElement

--- client/src/Components/Navbar/Navlink/index.stories.tsx
+++ client/src/Components/Navbar/Navlink/index.stories.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { Navlink, NavlinkProps } from '.'
+import { Navlink } from '.'
+import { RouterElement } from '../../../router'
 
 export default {
   title: 'Components/Navlink',
   component: Navlink
 } as Meta
 
-const Template: Story<NavlinkProps> = args => <Navlink {...args} />
+const Template: Story<RouterElement> = args => <Navlink {...args} />
 export const Example = Template.bind({})
 Example.args = {
-  page: 'Example',
-  route: 'Example/route'
+  name: 'Example',
+  path: 'Example/route',
+  component: undefined,
+  exact: true
 }
