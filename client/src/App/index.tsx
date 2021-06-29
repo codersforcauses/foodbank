a757194ebbe9f5187349aad539e2c0f852d630b0
Nicholas Choong <nicholascwc1997@gmail.com>
2021-06-29 20:36:01 +0800

    Removed comments and header tag

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,22 +1,12 @@
 import React from 'react'
-// import { Switch, Route } from 'react-router-dom'
 import routes from 'router'
 import { Navbar } from 'Components/Navbar'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
-      <header>
-        foodbank
-        <Navbar links={routes} />
-      </header>
+      <Navbar links={routes} />
     </div>
   )
 }
-
-// <Switch>
-//   {routes.map(({ name, ...route }) => (
-//     <Route key={name} {...route} />
-//   ))}
-// </Switch>
 export default App
