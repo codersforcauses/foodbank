6e5bbe689e4a68734885762617b745e5dd12cb2b
Nicholas Choong <nicholascwc1997@gmail.com>
2021-06-29 19:24:19 +0800

    Changed type from NavlinkProps to RouterElement

--- client/src/Components/Navbar/Navlink/index.tsx
+++ client/src/Components/Navbar/Navlink/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { RouterElement } from '../../../router'
+import { Route } from 'react-router-dom'
 
-export interface NavlinkProps {
-  page: string
-  route: string
+// export interface NavlinkProps {
+//   page: string
+//   route: string
+// }
+
+export const Navlink: React.FC<RouterElement> = ({ name, path, component }) => {
+  return <Route exact path={path} clasName={name} component={component} />
 }
 
-export const Navlink: React.FC<NavlinkProps> = ({ page, route }) => {
-  return (
-    <a className='text-white font-serif p-2 hover:opacity-75' href={route}>
-      {page}
-    </a>
-  )
-}
+// <a className='text-white font-serif p-2 hover:opacity-75' href={route}>
+//   {page}
+// </a>
