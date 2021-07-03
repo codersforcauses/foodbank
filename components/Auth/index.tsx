8f5c1f9201bf7cad704706af725620f37971600c
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-07-03 16:12:41 +0800

    add auth template

---
+++ components/Auth/index.tsx
@@ -0,0 +1,44 @@
+import { Button, Form, TextField, Modal } from '@components/Custom'
+
+interface AuthProps {
+  open: boolean
+  onClose: () => void
+}
+
+interface FormValues {
+  username: string
+  DOB: string
+}
+
+const defaultValues: FormValues = {
+  username: '',
+  DOB: ''
+}
+
+const Auth = (props: AuthProps) => {
+  return (
+    <Modal {...props} size='sm' heading='Sign-in'>
+      <Form<FormValues> defaultValues={defaultValues} onSubmit={values => console.log(values)}>
+        <TextField
+          label='Name'
+          name='username'
+        />
+        <TextField
+          label='Birth Year'
+          name='DOB'
+        />
+        <div className='flex justify-center pt-4'>
+          <Button className='flex items-center'>
+            Login
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" className="h-6 ml-8">
+              <path fill="#FFF" fillRule="evenodd" d="M12 0a12 12 0 100 25 12 12 0 000-25zm1 19v-5H6v-3h7V6l6 6-6 7z" />
+            </svg>
+          </Button>
+        </div>
+      </Form>
+    </Modal>
+  )
+}
+
+export default Auth
+export type LoginFormValues = keyof FormValues
---
+++ components/TownBox/index.tsx
@@ -0,0 +1,57 @@
+import './index.module.css'
+import Button, { ButtonProps } from '../Custom/Button'
+
+interface TownboxProps {
+  /**
+   * Header colour: enter either orange/primary
+   */
+  headerColor?: 'primary' | 'orange'
+
+  /**
+   * Heading contents
+   */
+  headerText?: string
+
+  /**
+   * Textbox contents
+   */
+  captionText?: string
+
+  /**
+   * Show visited button?
+   */
+  showButton?: boolean
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Townbox = ({
+  headerText = 'headerText',
+  captionText = 'captionText',
+  headerColor = 'orange',
+  showButton = false
+}: TownboxProps) => {
+  return (
+    <div className='flex w-tb1 md:w-tb2 lg:w-tb3 xl:w-tb4'>
+      <h2
+        className={`font-serif text-white p-2 px-4 border-black border-4 rounded-md ml-10 mt-4 absolute z-10 text-2xl md:text-3xl ${headerColor === 'primary' ? 'bg-primary' : 'bg-orange'}`}
+      >
+        {' '}{headerText}{' '}
+      </h2>
+      <div className='absolute h-64 mt-12 captionboxborder w-tb1 md:w-tb2 lg:w-tb3 xl:w-tb4'></div>
+      <div className='relative flex flex-col items-center justify-center w-full h-64 p-8 pt-2 pb-2 mt-12 border-black captionbox place-self-center'>
+        <p className='relative z-10 pb-2 mt-4 mb-0 font-sans text-base leading-5 break-words border-solid md:text-xl'>
+          {captionText}
+        </p>
+        {showButton && (
+          <Button
+            color={headerColor}
+          >
+            Visit
+          </Button>
+        )}
+      </div>
+    </div>
+  )
+}
