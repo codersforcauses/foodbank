812e71eabeb7f5e22161eb78f6ed5db368199652
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-07-03 15:37:44 +0800

    make navbar self-sustaining

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,20 +1,57 @@
+import { useCallback, useState } from 'react'
+import Link from 'next/link'
+import dynamic from 'next/dynamic'
 import NavLink, { NavLinkProps } from './NavLink'
 
-interface NavbarProps {
-  links: NavLinkProps[]
-}
+const Auth = dynamic(
+  () => import('../Auth'),
+  { ssr: false }
+)
 
-const Navbar = ({ links }: NavbarProps) => {
+const links: Array<NavLinkProps> = [
+  {
+    page: 'Superhero Foods',
+    route: '/'
+  },
+  {
+    page: 'Recipe',
+    route: '/'
+  },
+  {
+    page: 'Progress',
+    route: '/'
+  },
+]
+
+const Navbar = () => {
+  const [signIn, setSignIn] = useState(false)
+  const toggleSignIn = useCallback(() => {
+    setSignIn(prev => !prev)
+  }, [])
+
   return (
     <header className='fixed inset-x-0 top-0 z-10 py-3 bg-primary'>
-      <nav className='container flex justify-center px-3 mx-auto space-x-10'>
-        {links.map(nav => (
-          <NavLink key={nav.page} {...nav} />
-        ))}
-      </nav>
+      <div className="container flex justify-between px-3 mx-auto">
+        <Link href='/'>
+          <a className='px-4 py-1 font-serif text-xl text-white hover:opacity-75'>
+            Home
+          </a>
+        </Link>
+        <nav className='flex items-center space-x-10'>
+          {links.map(nav => (
+            <NavLink key={nav.page} {...nav} />
+          ))}
+        </nav>
+        <button
+          className='px-4 py-1 font-serif text-xl text-white hover:opacity-75'
+          onClick={toggleSignIn}
+        >
+          Sign-in
+        </button>
+      </div>
+      <Auth open={signIn} onClose={toggleSignIn} />
     </header>
   )
 }
 
 export default Navbar
-export type {NavLinkProps}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,11 @@
 import type { AppProps } from 'next/app'
-import Navbar, { NavLinkProps } from '@components/NavBar'
+import Navbar from '@components/NavBar'
 import '@styles/main.css'
-
-const linkList: NavLinkProps[] = [
-  {
-    page: 'Home',
-    route: '/'
-  },
-  {
-    page: 'Superhero Foods',
-    route: '/'
-  },
-  {
-    page: 'Recipe',
-    route: '/'
-  },
-  {
-    page: 'Progress',
-    route: '/'
-  },
-]
 
 const FoodBank = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Navbar links={linkList} />
+      <Navbar />
       <main className='h-full md:mt-16'>
         <Component {...pageProps} />
       </main>
