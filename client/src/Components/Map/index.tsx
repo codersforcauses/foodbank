49631aa575d6da896316efe5fb15357253972674
Mia <miablessas@gmail.com>
2021-07-03 15:21:45 +0800

    added imagemap

--- client/src/Components/Map/index.tsx
+++ client/src/Components/Map/index.tsx
@@ -6,10 +6,12 @@ import './index.css'
 import { Location } from '../../lib/types'
 import svgData from './svgImageData.json'
 import SVGLocationGroup from './SVGLocationGroup'
+import mapImg from './assets/TuckerMap.jpg'
 import descData from './assets/description.json'
 const Map: React.FC = () => {
   // Used because SVG does not scale properly without
   const [height, setHeight] = useState(0)
+  const [width, setWidth] = useState(0)
   const elementRef = useRef(null as null | HTMLDivElement)
 
   const [selected, onSelect] = useState<Location | null>(null)
@@ -18,12 +20,20 @@ const Map: React.FC = () => {
   const [showButton, setButton] = useState<boolean>(false)
   const [maxWidth, setMaxWidth] = useState<string>("200")
   const [maxHeight, setMaxHeight] = useState<string>("100")
+  const [scale, setScale] = useState(1)
   
   useEffect(() => {
     if (elementRef?.current?.clientHeight) {
       setHeight(elementRef?.current?.clientHeight)
     }
   }, []) //empty dependency array so it only runs once at render
+
+  useEffect(() => {
+    if (elementRef?.current?.clientWidth) {
+      setWidth(elementRef?.current?.clientWidth)
+      setScale(width/4961)
+    }
+  }, [width])
 
   useEffect(() => {
     if(selected != null){
@@ -63,6 +73,12 @@ const Map: React.FC = () => {
     selected === area ? onSelect(null) : onSelect(area)
   }
 
+  const handleClick = (event:any) => { //need to change this type
+    event.preventDefault()
+    const area = event.target.alt
+    onMapClick(Location[area as keyof typeof Location])
+  }
+
   // Data can be made from dev/svgParse.py
   return (
     <div
@@ -71,6 +87,32 @@ const Map: React.FC = () => {
     >
       {height === 0 ? null : (
         <div className='svgrow'>
+          <img src={mapImg} alt="Tucker Island Map" useMap="#tuckerislandmap"/>
+          <map name="tuckerislandmap">
+          {
+            svgData.groupArray.map(location => {
+              if (location.coords) {
+              const scaledCoords = location.coords.map(coord => coord*scale)
+
+              return (
+                <area
+                key={location.id}
+                alt={location.id}
+                onClick={handleClick}
+                href={location.id}
+                coords={scaledCoords.join()}
+                className={
+                  Location[location.id as keyof typeof Location] === selected
+                    ? 'map-selected'
+                    : 'map-unselected'
+                }
+                shape="poly"
+                  />
+              )
+              }
+            })
+          }
+            </map>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             xmlnsXlink='http://www.w3.org/1999/xlink'
