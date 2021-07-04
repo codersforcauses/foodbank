d1474340005ac22a5b931995de95eb35fddd2a2d
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-07-04 11:28:36 +0800

    achievements loaded onto page via hooks

--- pages/achievements.tsx
+++ pages/achievements.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import type { Achievement } from 'lib/types'
 import blackboard from 'public/images/blackboard.jpg'
+import allAchievements from 'lib/achievements'
 
 const Achievements: React.FC = () => {
+  const [achievements, setAchievements] = useState<Array<Achievement>>([])
+
+  useEffect(() => {
+    setAchievements(allAchievements)
+  }, [])
+
   return (
     <div>
       <Image src={blackboard} alt='Blackboard' />
+      <div>
+        {achievements.map(achievement => (
+          <Image
+            src={achievement.image}
+            alt='Achievement'
+            width={150}
+            height={150}
+            key={achievement.title}
+          />
+        ))}
+      </div>
     </div>
   )
 }
