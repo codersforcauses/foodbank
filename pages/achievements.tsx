2cb0d52fc4021f066ffd09de16dba49169e41bb3
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-07-04 11:46:44 +0800

    added grid layout, achievements titles, unlocked/locked styling

--- pages/achievements.tsx
+++ pages/achievements.tsx
@@ -8,21 +8,28 @@ const Achievements: React.FC = () => {
   const [achievements, setAchievements] = useState<Array<Achievement>>([])
 
   useEffect(() => {
+    allAchievements[0].unlocked = true
+    allAchievements[2].unlocked = true
     setAchievements(allAchievements)
   }, [])
 
   return (
-    <div>
-      <Image src={blackboard} alt='Blackboard' />
-      <div>
-        {achievements.map(achievement => (
-          <Image
-            src={achievement.image}
-            alt='Achievement'
-            width={150}
-            height={150}
-            key={achievement.title}
-          />
+    <div className='relative'>
+      <div className='absolute'>
+        <Image src={blackboard} alt='Blackboard' />
+      </div>
+      <div className='absolute m-28 grid grid-cols-5 gap-16'>
+        {achievements.map(({ title, image, unlocked }) => (
+          <div
+            key={title}
+            className={
+              'grid grid-cols-1 gap-y-6 place-items-center ' +
+              (unlocked ? '' : 'opacity-20')
+            }
+          >
+            <Image src={image} alt='Achievement' width={150} height={150} />
+            <div>{title}</div>
+          </div>
         ))}
       </div>
     </div>
