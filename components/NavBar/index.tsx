1b36c91c13868626f76c6d9ea624ec795a744130
Nicholas Choong <nicholascwc1997@gmail.com>
2021-07-05 19:31:14 +0800

    Added logo and dynamic signin/signout button

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -2,25 +2,24 @@ import { useCallback, useState } from 'react'
 import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import NavLink, { NavLinkProps } from './NavLink'
+import Image from 'next/image'
+import logo from '../../public/images/tuckerMap.webp' //placeholder
 
-const Auth = dynamic(
-  () => import('../Auth'),
-  { ssr: false }
-)
+const Auth = dynamic(() => import('../Auth'), { ssr: false })
 
 const links: Array<NavLinkProps> = [
   {
-    page: 'Superhero Foods',
-    route: '/'
+    page: 'SUPERHERO FOODS',
+    route: '/super_foods' //placeholder
   },
   {
-    page: 'Recipe',
-    route: '/'
+    page: 'RECIPE',
+    route: '/recipe' //placeholder
   },
   {
-    page: 'Progress',
-    route: '/'
-  },
+    page: 'PROGRESS',
+    route: '/progress' //placeholder
+  }
 ]
 
 const Navbar = () => {
@@ -29,25 +28,43 @@ const Navbar = () => {
     setSignIn(prev => !prev)
   }, [])
 
+  const logoSize: number = 50
+
   return (
     <header className='fixed inset-x-0 top-0 z-10 py-3 bg-primary'>
-      <div className="container flex justify-between px-3 mx-auto">
+      <div className='container flex justify-between px-3 mx-auto'>
         <Link href='/'>
-          <a className='px-4 py-1 font-serif text-xl text-white hover:opacity-75'>
-            Home
+          <a className='inline-block w-12 h-1 hover:opacity-75'>
+            <Image
+              className='object-fill w-full h-full'
+              src={logo}
+              alt='Foodbank logo'
+              placeholder='blur'
+              //   width={logoSize}
+              //   height={logoSize}
+            />
           </a>
         </Link>
-        <nav className='flex items-center space-x-10'>
+        <nav className='flex items-center space-x-10' role='navigation'>
           {links.map(nav => (
             <NavLink key={nav.page} {...nav} />
           ))}
         </nav>
-        <button
-          className='px-4 py-1 font-serif text-xl text-white hover:opacity-75'
-          onClick={toggleSignIn}
-        >
-          Sign-in
-        </button>
+        {signIn ? (
+          <button
+            className='px-4 py-1 font-serif text-xl text-white hover:opacity-75'
+            onClick={toggleSignIn}
+          >
+            SIGN-OUT
+          </button>
+        ) : (
+          <button
+            className='px-4 py-1 font-serif text-xl text-white hover:opacity-75'
+            onClick={toggleSignIn}
+          >
+            SIGN-IN
+          </button>
+        )}
       </div>
       <Auth open={signIn} onClose={toggleSignIn} />
     </header>
