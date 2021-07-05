4a892e9c77f965e005478dc9f96d315772609982
Nicholas Choong <nicholascwc1997@gmail.com>
2021-07-05 20:05:10 +0800

    Added foodbank logo

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import NavLink, { NavLinkProps } from './NavLink'
 import Image from 'next/image'
-import logo from '../../public/images/tuckerMap.webp' //placeholder
+import logo from '../../public/images/logoFoodbankAus.webp'
 
 const Auth = dynamic(() => import('../Auth'), { ssr: false })
 
@@ -28,20 +28,16 @@ const Navbar = () => {
     setSignIn(prev => !prev)
   }, [])
 
-  const logoSize: number = 50
-
   return (
     <header className='fixed inset-x-0 top-0 z-10 py-3 bg-primary'>
       <div className='container flex justify-between px-3 mx-auto'>
         <Link href='/'>
-          <a className='inline-block w-12 h-1 hover:opacity-75'>
+          <a className='hover:opacity-75'>
             <Image
-              className='object-fill w-full h-full'
+              className='w-full h-full'
               src={logo}
               alt='Foodbank logo'
               placeholder='blur'
-              //   width={logoSize}
-              //   height={logoSize}
             />
           </a>
         </Link>
