d721cce02428b16ab68266a3b5c50cd2f17ad421
Nicholas Choong <nicholascwc1997@gmail.com>
2021-07-13 06:02:41 +0800

    For testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import Map from '@components/Map'
+import Characters from '@components/Grid/Characters'
+import UsernameForm from '@components/Grid/UsernameForm'
 
-const Home = () => <Map />
-
+const Home = () => {
+  return (
+    <>
+      <UsernameForm />
+      {/* <Characters seed='Hello' /> */}
+    </>
+  )
+}
 export default Home
