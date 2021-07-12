45041251d26674d7d5105d0798833ce100ebb1aa
Nicholas Choong <nicholascwc1997@gmail.com>
2021-07-13 03:53:52 +0800

    Added username form

---
+++ components/UsernameForm/index.tsx
@@ -0,0 +1,57 @@
+import { useState, useRef, useEffect, ChangeEvent } from 'react'
+import Characters from '@components/Characters'
+
+const UsernameForm = () => {
+  const [username, setUsername] = useState('')
+  const [selectedCount, setSelectedCount] = useState(0)
+  const [grid, setGrid] = useState([])
+
+  const focusRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    if (focusRef.current !== null) {
+      focusRef.current.focus()
+    }
+  })
+
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }
+  const handleUsernameSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setUsername(username)
+    alert(username)
+  }
+
+  return (
+    <>
+      <form
+        className='flex content-center justify-center pt-60'
+        onSubmit={handleUsernameSubmit}
+      >
+        <input
+          type='text'
+          placeholder={'Username'}
+          value={username}
+          name='text'
+          className=''
+          onChange={handleUsernameChange}
+          ref={focusRef}
+        />
+        <button className=''>YOU SURE???</button>
+      </form>
+      {username ? (
+        <>
+          <p className='flex content-center justify-center pt-40'>
+            Username: &emsp;
+            {username}
+          </p>
+          {/* <Characters seed={username} /> */}
+        </>
+      ) : (
+        ''
+      )}
+    </>
+  )
+}
+
+export default UsernameForm
