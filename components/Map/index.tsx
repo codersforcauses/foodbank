93a572d106fdfbc7eb9df7d0f53d278d60d16925
kurapikaaaa <petersheng2@outlook.com>
2021-07-14 22:04:29 +0800

    works

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,16 +1,163 @@
-import Image from 'next/image'
-import baseMap from '../../public/images/tuckerMap.webp'
+// naming conventions of items in svg = id of group in camelCase, image import name in PascalCase.
+// svg tree generated from dev/svgParse.py (super hacky atm)
 
-const Map = () => (
-  <Image
-    priority
-    src={baseMap}
-    alt='Tucker Island Map'
-    placeholder='blur'
-    layout='fill'
-    objectFit='cover'
-    objectPosition='left center'
-  />
-)
+import React, { useEffect, useRef, useState } from 'react'
+// import { Menu } from '@headlessui/react'
+import './index.css'
+import { Location } from '../../lib/types'
+import svgData from './svgImageData.json'
+import mapImg from './assets/TuckerMap.jpg'
+import descData from './assets/description.json'
+import Townbox from '../TownBox'
+const Map: React.FC = () => {
+  // Used because SVG does not scale properly without
+  const [height, setHeight] = useState(1)
+  const elementRef = useRef(null as null | HTMLDivElement)
+  const [selected, onSelect] = useState<Location | null>(null)
+  const [scale, setScale] = useState(1)
+  // const [townbox, setTownbox] = useState(<></>)
+  type HeaderColor = 'primary' | 'orange';
+  useEffect(() => {
+    if (elementRef?.current?.clientHeight) {
+      setHeight(elementRef?.current?.clientHeight)
+    }
+  }, []) //empty dependency array so it only runs once at render
+
+  useEffect(() => {
+    function handleResize() {
+      const mobileWidth = 2900
+      const minStaticWidth = 1170
+      const targetWidth = window.innerWidth > minStaticWidth ? window.innerWidth : mobileWidth
+      setScale(targetWidth/4961)
+    }
+    handleResize();
+    window.addEventListener('resize', handleResize)
+    },
+    []
+  )
+
+  // to get the area description given an area so you can actually use headers/captions
+  // returns null if such area doesn't exist in assets/description.json
+  const getAreaDescription = (area: Location) => {
+    for(const description of descData.descriptionArray) {
+      if (description.id === Location[area]) {
+        return description
+      }
+    }
+
+    return null
+  }
+  const onClose = () => {
+    onSelect(null) 
+  }
+  const onMapClick = (area: Location) => {
+    selected === area ? onSelect(null) : onSelect(area)
+  }
+  // eslint-disable-next-line
+  const handleClick = (event: any) => { //need to change this type
+    event.preventDefault()
+    const area = event.target.alt
+    onMapClick(Location[area as keyof typeof Location])
+  }
+
+
+  // Data can be made from dev/svgParse.py
+  return (
+    <div
+      ref={elementRef}
+      className='block'
+    >
+      {height === 0 ? null : (
+          <div className='svgrow'>
+              <img src={mapImg} alt="Tucker Island Map" className="map" useMap="#tuckerislandmap"/>
+            <map name="tuckerislandmap">
+              {
+                svgData.groupArray.map(location => {
+                  if (location.coords){
+                    const scaledCoords = location.coords.map(coord => coord*scale)
+                    const className = Location[location.id as keyof typeof Location] === selected
+                          ? 'map-selected'
+                          : 'map-unselected'
+                    return (
+                        <area 
+                          key={location.id}
+                          alt={location.id}
+                          onClick={handleClick}
+                          href={location.id}
+                          coords={scaledCoords.join()}
+                          className={className}
+                          shape="poly"
+                        />
+                    )
+                  }
+                })
+              }
+            </map>
+
+            {
+                svgData.groupArray.map(area => {
+                  if (selected !== null && area.coords){
+                    const selectedArea = getAreaDescription(selected)
+
+                    if(selectedArea !== null && selectedArea.id === area.id) {
+                      const header = selectedArea?.headerText
+                      const caption = selectedArea?.captionText
+                      const showButton = selectedArea?.showButton
+                      
+                      const headerColor:HeaderColor = selectedArea?.headerColor as HeaderColor;
+
+                      const maxWidth = selectedArea?.maxWidth
+                      const maxHeight = selectedArea?.maxHeight
+
+                      const xtrans = parseInt(area.xtrans) * scale * 8; // I have no clue why everything is overscaled 8x
+                      const ytrans = parseInt(area.ytrans) * scale * 8; // this is probably worth looking into
+                      const translation = "translate(" + String(xtrans) + "px, " + String(ytrans) + "px)";
+                      const up = ["aquaOcean", "zombieWasteland", "grainField"]
+                      const left = ["yoghurtMountains", "cluckyCoop", "grainField", "supplyStore", "wickedWaterway"]
+
+                      console.log("area",selectedArea);
+                      console.log(area.id)
+                      console.log(translation)
+                      return (
+                        <div key={area.id} style={{transform:translation}}>
+                          <div className={`townBox ${up.includes(area.id)? "up" : ""} ${left.includes(area.id)? "left " : ""}`}>
+                            <Townbox 
+                              maxWidth={maxWidth} 
+                              maxHeight={maxHeight}
+                              headerColor={headerColor}
+                              headerText={header}
+                              captionText={caption}
+                              showButton = {showButton}
+                              close={onClose}
+                            />
+                          </div>
+                        </div>
+                      )
+                    }
+                  }
+                })
+              }
+
+            {svgData.groupArray.map(location =>{
+                const xtrans = parseInt(location.xtrans) * scale * 8; // I have no clue why everything is overscaled 8x
+                const ytrans = parseInt(location.ytrans) * scale * 8; // this is probably worth looking into
+                const translation = "translate(" + String(xtrans) + "px, " + String(ytrans) + "px)";
+                const up = ["aquaOcean", "zombieWasteland", "grainField"]
+                const left = ["yoghurtMountains", "cluckyCoop", "grainField", "supplyStore", "wickedWaterway"]
+                return (
+                  <div key={location.id} style={{position:"absolute", zIndex:4, top:0, transform:translation}}>
+                    {
+                      selected === Location[location.id as keyof typeof Location] &&
+                      <div className={`townBox ${up.includes(location.id)? "up" : ""} ${left.includes(location.id)? "left " : ""}`}></div>
+                    }
+                  </div>
+                )
+              })}
+
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Map
