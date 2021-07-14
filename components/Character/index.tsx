3869d9bb0a418f4e35c9c71554e1d2870327ab27
Jamie McCullough <66285033+jmemcc@users.noreply.github.com>
2021-07-14 19:07:29 +0800

    Minor grid alterations

--- components/Character/index.tsx
+++ components/Character/index.tsx
@@ -1,16 +1,17 @@
 //import './index.css';
-import chars from './characters.json';
+import chars from './characters.json'
+import Image from 'next/image'
 
 interface CharacterProps {
-  baseName: string;
-  aliasName: string;
-  baseImageUrl: string;
-  aliasImageUrl: string;
-  about: string;
-  recipe: string;  
-  superPowers: string;
-  foodGroup: string;
-  location: string;
+  baseName: string
+  aliasName: string
+  baseImageUrl: string
+  aliasImageUrl: string
+  about: string
+  recipe: string
+  superPowers: string
+  foodGroup: string
+  location: string
 }
 
 const Character: React.FC<CharacterProps> = ({
@@ -22,37 +23,39 @@ const Character: React.FC<CharacterProps> = ({
   recipe,
   superPowers,
   foodGroup,
-  location,
- }) => {
-
+  location
+}) => {
   // const [isTransformed, setIsTransformed] = useState(false);
   // const getName = () => isTransformed ? aliasName : baseName;
   // const getImageUrl = () => isTransformed ? aliasImageUrl : baseImageUrl;
 
   return (
-    <div className="main-window">
-      <button className="bg-primary"></button>
-      <div className="character-card-container">
-        <div className="character-image-container">
-          <img src={baseImageUrl} alt={baseName}></img>
+    <div className='main-window'>
+      <button className='bg-primary'></button>
+      <div className='character-card-container'>
+        <div className='character-image-container'>
+          <p>
+            {' '}
+            img src={baseImageUrl} alt={baseName} /img{' '}
+          </p>
         </div>
         <button>Activate super form!</button>
       </div>
-      <div className="info-window">
-        <div className="name-window">{baseName}</div>
-        <div className="about-window">{about}</div>
-        <div className="recipe-window">{recipe}</div>
-        <div className="superpowers-window">{superPowers}</div>
-        <div className="foodgroup-window">{foodGroup}</div>
-        <div className="location">{location}</div>
-        <div className="navigation">
+      <div className='info-window'>
+        <div className='name-window'>{baseName}</div>
+        <div className='about-window'>{about}</div>
+        <div className='recipe-window'>{recipe}</div>
+        <div className='superpowers-window'>{superPowers}</div>
+        <div className='foodgroup-window'>{foodGroup}</div>
+        <div className='location'>{location}</div>
+        <div className='navigation'>
           <button>Previous</button>
           <button>Grid</button>
           <button>Next</button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Character;
+export default Character
--- components/Grid/index.tsx
+++ components/Grid/index.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from 'react'
 import Image from 'next/image'
-import chars from '@components/Character/characters.json';
+import chars from '@components/Character/characters.json'
 
 // seems to be a problem here with using map
 // https://sentry.io/answers/unique-key-prop/
 
 const Grid = () => (
-  <div className="grid">
-    {chars.superhero.map(char => 
-      <div className="grid-cell" key={char.name}>
-        <div className="grid-name">
-          {char.aliasName}
-        </div>
-        <Image 
-          className="grid-image"
+  <div className='grid'>
+    {chars.superhero.map(char => (
+      <div className='grid-cell' key={char.name}>
+        <div className='grid-name'>{char.aliasName}</div>
+        <Image
+          className='grid-image'
           src={char.baseImage}
-          width="100px"
-          height="100px"/>
-      </div>)}
+          alt={char.name}
+          width='100px'
+          height='100px'
+        />
+      </div>
+    ))}
   </div>
-  
-  )
+)
 
-export default Grid;
-
+export default Grid
