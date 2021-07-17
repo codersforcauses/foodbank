c15dd4bc880f119a688aef88a510dae4075513e2
Nicholas Choong <nicholascwc1997@gmail.com>
2021-07-17 18:53:01 +0800

    onSubmit is working but not onChange

--- components/Auth/index.tsx
+++ components/Auth/index.tsx
@@ -1,5 +1,15 @@
+import {
+  useState,
+  useRef,
+  useEffect,
+  ChangeEvent,
+  MouseEventHandler
+} from 'react'
 import { Button, Form, TextField, Modal } from '@components/Custom'
+import GridDisplay, { selectSet, Character } from '@components/Grid/GridForm'
+import { SubmitHandler } from 'react-hook-form'
 
+const CHARACTERS_FOR_AUTH = 3
 interface AuthProps {
   open: boolean
   onClose: () => void
@@ -7,23 +17,73 @@ interface AuthProps {
 
 interface FormValues {
   username: string
-  DOB: string
+  password: string
 }
 
 const defaultValues: FormValues = {
   username: '',
-  DOB: ''
+  password: ''
 }
 
 const Auth = (props: AuthProps) => {
+  const [input, setInput] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [grid, setGrid] = useState<Character[]>([])
+  const [selectedCount, setSelectedCount] = useState(0)
+
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+    console.log(e.target.value)
+    setGrid([])
+  }
+
+  const handleUsernameSubmit: SubmitHandler<FormValues> = (
+    value: FormValues
+  ) => {
+    // e.preventDefault()
+    setUsername(value.username)
+    setGrid(selectSet(value.username))
+    setPassword('')
+    setSelectedCount(0)
+    console.log(value.username)
+  }
+
+  const toggleSelect = (id: string) => {
+    const newGrid: Array<Character> = JSON.parse(JSON.stringify(grid))
+    const character = newGrid.find(char => char.id === id)
+    if (character) {
+      if (character.isSelected) {
+        setSelectedCount(prevCount => prevCount - 1)
+      } else {
+        setSelectedCount(prevCount => prevCount + 1)
+      }
+      character.isSelected = !character.isSelected
+    }
+    setGrid(newGrid)
+  }
+
+  const handlePasswordSubmit = () => {
+    if (selectedCount !== CHARACTERS_FOR_AUTH) {
+      return
+    }
+    const newFilteredGrid = [...grid].filter(char => char.isSelected)
+    const newPassword = newFilteredGrid.map(char => char.password).join('')
+    setPassword(newPassword)
+  }
+
   return (
     <Modal {...props} size='sm' heading='Sign-in'>
       <Form<FormValues>
         defaultValues={defaultValues}
-        onSubmit={values => console.log(values)}
+        onSubmit={handleUsernameSubmit}
       >
-        <TextField label='Name' name='username' />
-        <TextField label='Birth Year' name='DOB' />
+        <TextField
+          label='Name'
+          name='username'
+          onChange={handleUsernameChange}
+        />
+        <p>{input}</p>
         <div className='flex justify-center pt-4'>
           <Button className='flex items-center'>
             Login
