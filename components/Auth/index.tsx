263d9e99943fe4047de0da2bb041fa0c015ed69d
Nicholas Choong <nicholascwc1997@gmail.com>
2021-07-19 16:40:36 +0800

    A working auth grid

--- components/Auth/index.tsx
+++ components/Auth/index.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, MouseEventHandler } from 'react'
 import { SubmitHandler } from 'react-hook-form'
 import { Button, Form, TextField, Modal } from '@components/Custom'
 import { selectSet, Character, GridField } from '@components/Grid/GridForm'
@@ -20,130 +20,141 @@ const defaultValues: FormValues = {
 }
 
 const Auth = (props: AuthProps) => {
-  //   const [input, setInput] = useState('')
-  //   const [username, setUsername] = useState('')
-  //   const [password, setPassword] = useState('')
-  //   const [grid, setGrid] = useState<Character[]>([])
-  //   const [selectedCount, setSelectedCount] = useState(0)
+  const [input, setInput] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [grid, setGrid] = useState<Character[]>([])
+  const [selectedCount, setSelectedCount] = useState(0)
 
-  //   const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //     setInput(e.target.value)
-  //     setGrid([])
-  //   }
+  const handleUsernameChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    values: 'username'
+  ) => {
+    setInput(e.target.value)
+    console.log(e.target.value)
+    console.log(JSON.stringify(defaultValues))
+  }
 
-  //   const handleUsernameSubmit: SubmitHandler<FormValues> = value => {
-  //     setUsername(value.username)
-  //     setGrid(selectSet(value.username))
-  //     setPassword('')
-  //     setSelectedCount(0)
-  //     console.log(defaultValues)
-  //   }
+  const handleUsernameSubmit: MouseEventHandler<HTMLButtonElement> = () => {
+    setUsername(input)
+    setGrid(selectSet(input))
+    setPassword('')
+    setSelectedCount(0)
+    console.log(input)
+    console.log(defaultValues)
+  }
 
-  //   const toggleSelect = (id: string) => {
-  //     const newGrid: Array<Character> = JSON.parse(JSON.stringify(grid))
-  //     const character = newGrid.find(char => char.id === id)
-  //     if (character) {
-  //       if (character.isSelected) {
-  //         setSelectedCount(prevCount => prevCount - 1)
-  //       } else {
-  //         setSelectedCount(prevCount => prevCount + 1)
-  //       }
-  //       character.isSelected = !character.isSelected
-  //     }
-  //     setGrid(newGrid)
-  //   }
+  const toggleSelect = (id: string) => {
+    const newGrid: Array<Character> = JSON.parse(JSON.stringify(grid))
+    const character = newGrid.find(char => char.id === id)
+    if (character) {
+      if (character.isSelected) {
+        setSelectedCount(prevCount => prevCount - 1)
+      } else {
+        setSelectedCount(prevCount => prevCount + 1)
+      }
+      character.isSelected = !character.isSelected
+    }
+    setGrid(newGrid)
+  }
 
-  //   const handlePasswordSubmit: SubmitHandler<FormValues> = value => {
-  //     // if (selectedCount !== CHARACTERS_FOR_AUTH) {
-  //     //   return
-  //     // }
-  //     if (value.password.length !== CHARACTERS_FOR_AUTH) {
-  //       return
-  //     }
-  //     // const newFilteredGrid = [...grid].filter(char => char.isSelected)
-  //     // const newPassword = newFilteredGrid.map(char => char.password).join('')
-  //     const newPassword = value.password.map(char => char.password).join('')
-  //     setPassword(newPassword)
-  //     console.log(value)
-  //     console.log(newPassword)
-  //     console.log(defaultValues)
-  //   }
+  const handlePasswordSubmit: SubmitHandler<FormValues> = value => {
+    if (selectedCount !== CHARACTERS_FOR_AUTH) {
+      return
+    }
+    // if (value.password.length !== CHARACTERS_FOR_AUTH) {
+    //   return
+    // }
+    const newPassword = value.password.join('')
+    setPassword(newPassword)
+    console.log(value.password)
+    console.log(newPassword)
+    console.log(defaultValues)
+  }
 
   return (
     <Modal {...props} size='sm' heading='Sign-in'>
-      {/* {!username ? ( */}
       <Form<FormValues>
         defaultValues={defaultValues}
-        // onSubmit={value => handleUsernameSubmit(value)}
-        onSubmit={value => console.log(value)}
+        onSubmit={handlePasswordSubmit}
       >
-        <TextField label='Name' type='text' name='username' />
-        <div className='flex justify-center pt-4'>
-          <Button className='flex items-center'>
-            Set Username
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 25 25'
-              className='h-6 ml-8'
-            >
-              <path
-                fill='#FFF'
-                fillRule='evenodd'
-                d='M12 0a12 12 0 100 25 12 12 0 000-25zm1 19v-5H6v-3h7V6l6 6-6 7z'
-              />
-            </svg>
-          </Button>
-        </div>
+        {!username ? (
+          <>
+            <TextField
+              label='Name'
+              type='text'
+              name='username'
+              value={input}
+              onChange={e => handleUsernameChange(e, 'username')}
+            />
+            <div className='flex justify-center pt-4'>
+              <Button
+                className='flex items-center'
+                type='button'
+                onClick={handleUsernameSubmit}
+              >
+                Set Username
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 25 25'
+                  className='h-6 ml-8'
+                >
+                  <path
+                    fill='#FFF'
+                    fillRule='evenodd'
+                    d='M12 0a12 12 0 100 25 12 12 0 000-25zm1 19v-5H6v-3h7V6l6 6-6 7z'
+                  />
+                </svg>
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            {/* <TextField label='' type='hidden' name='username' value={username} /> */}
+            <GridField
+              label='grid'
+              type='checkbox'
+              name='password'
+              grid={grid}
+              toggleSelect={toggleSelect}
+            />
+            <div className='flex justify-center pt-4'>
+              <Button
+                type='button'
+                onClick={() => setUsername('')}
+                className='flex items-center'
+              >
+                Back
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 25 25'
+                  className='h-6 ml-8'
+                >
+                  <path
+                    fill='#FFF'
+                    fillRule='evenodd'
+                    d='M12 0a12 12 0 100 25 12 12 0 000-25zm1 19v-5H6v-3h7V6l6 6-6 7z'
+                  />
+                </svg>
+              </Button>
+              <Button className='flex items-center'>
+                Confirm Selections
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 25 25'
+                  className='h-6 ml-8'
+                >
+                  <path
+                    fill='#FFF'
+                    fillRule='evenodd'
+                    d='M12 0a12 12 0 100 25 12 12 0 000-25zm1 19v-5H6v-3h7V6l6 6-6 7z'
+                  />
+                </svg>
+              </Button>
+            </div>
+          </>
+        )}
       </Form>
-      {/* ) : (
-        <Form<FormValues>
-          defaultValues={defaultValues}
-          onSubmit={value => handlePasswordSubmit(value)}
-        >
-          <TextField label='' type='hidden' name='username' value={username} />
-          <GridField
-            label='grid'
-            type='checkbox'
-            name='password'
-            grid={grid}
-            toggleSelect={toggleSelect}
-          />
-          <div className='flex justify-center pt-4'>
-            <Button
-              type='button'
-              onClick={() => setUsername('')}
-              className='flex items-center'
-            >
-              Back
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 25 25'
-                className='h-6 ml-8'
-              >
-                <path
-                  fill='#FFF'
-                  fillRule='evenodd'
-                  d='M12 0a12 12 0 100 25 12 12 0 000-25zm1 19v-5H6v-3h7V6l6 6-6 7z'
-                />
-              </svg>
-            </Button>
-            <Button className='flex items-center'>
-              Confirm Selections
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 25 25'
-                className='h-6 ml-8'
-              >
-                <path
-                  fill='#FFF'
-                  fillRule='evenodd'
-                  d='M12 0a12 12 0 100 25 12 12 0 000-25zm1 19v-5H6v-3h7V6l6 6-6 7z'
-                />
-              </svg>
-            </Button>
-          </div>
-        </Form>
-      )} */}
     </Modal>
   )
 }
