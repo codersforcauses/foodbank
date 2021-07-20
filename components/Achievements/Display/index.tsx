e89a866b3daeed143814b8c9ca8f5fc93270d3ea
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-07-20 12:42:12 +0800

    removed explosion and logo fill effect, cleaner css code

--- components/Achievements/Display/index.tsx
+++ components/Achievements/Display/index.tsx
@@ -19,22 +19,9 @@ const Display: React.FC = () => {
             <div className='relative'>
               <div
                 className={
-                  'm-auto w-40 h-40 filter ' +
-                  (unlocked ? '' : 'grayscale contrast-50 opacity-50')
+                  'm-auto w-20 h-20 left-12 top-9 filter ' +
+                  (unlocked || 'grayscale contrast-50 opacity-50')
                 }
-              >
-                <Image src={explosion} alt='Explosion' />
-              </div>
-              <div className='absolute m-auto w-20 h-20 left-12 top-9 filter grayscale contrast-50 opacity-50'>
-                <Image src={image} alt='Achievement' width={70} height={70} />
-              </div>
-              <div
-                className='absolute m-auto w-20 h-20 left-12 top-9'
-                style={{
-                  clipPath: `polygon(0% 100%, 100% 100%, 100% ${
-                    100 - (progress / total) * 100
-                  }%, 0% ${100 - (progress / total) * 100}%)`
-                }}
               >
                 <Image src={image} alt='Achievement' width={70} height={70} />
               </div>
@@ -42,7 +29,7 @@ const Display: React.FC = () => {
             <div
               className={
                 'font-serif text-center ' +
-                (unlocked ? '' : 'grayscale contrast-50 opacity-50')
+                (unlocked || 'grayscale contrast-50 opacity-50')
               }
             >
               <div>{title}</div>
