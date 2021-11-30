32d868ff734294df6471f25773cf4cd716400113
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-12-01 00:01:49 +0800

    update keenslider api

--- components/Custom/Carousel/index.tsx
+++ components/Custom/Carousel/index.tsx
@@ -1,11 +1,10 @@
 import { PropsWithChildren, useState, useEffect, useRef } from 'react'
-import { useKeenSlider } from 'keen-slider/react'
-import { TOptionsEvents } from 'keen-slider'
+import { KeenSliderOptions, useKeenSlider } from 'keen-slider/react'
 import Arrow from './Arrow'
 import Dots from './Dots'
 import 'keen-slider/keen-slider.min.css'
 
-export interface CarouselProps extends TOptionsEvents {
+export interface CarouselProps extends KeenSliderOptions {
   autoplay?: boolean
   autoplayDuration?: number
   controls?: boolean
@@ -24,25 +23,33 @@ const Carousel = ({
   autoplayDuration = 2000,
   ...props
 }: PropsWithChildren<CarouselProps>) => {
-  const [ref, slider] = useKeenSlider<HTMLDivElement>({
-    ...props,
-    slideChanged(slide) {
-      setCurrentSlide(slide.details().relativeSlide)
-    }
-  })
+  const timer = useRef<NodeJS.Timer | undefined>(undefined)
   const [currentSlide, setCurrentSlide] = useState(0)
   const [pause, setPause] = useState(false)
-  const timer = useRef<NodeJS.Timer | undefined>(undefined)
+  const [ref, slider] = useKeenSlider<HTMLDivElement>({
+    ...props,
+    created(s) {
+      s.container.addEventListener('mouseover', () => {
+        setPause(true)
+      })
+      s.container.addEventListener('mouseout', () => {
+        setPause(false)
+      })
+    },
+    slideChanged(slide) {
+      setCurrentSlide(slide.track.details.rel)
+    }
+  })
 
   const moveToSlide = (idx: number) => {
-    slider.moveToSlide(idx)
+    slider.current?.moveToIdx(idx)
     setCurrentSlide(idx)
   }
 
   useEffect(() => {
     timer.current = setInterval(() => {
       if (!pause && slider && autoplay) {
-        slider.next()
+        slider.current?.next()
       }
     }, autoplayDuration)
     return () => {
@@ -51,24 +58,6 @@ const Carousel = ({
       }
     }
   }, [slider, pause, autoplayDuration, autoplay])
-
-  useEffect(() => {
-    const listener = ref.current
-    const pause = () => {
-      setPause(true)
-    }
-    const unpause = () => {
-      setPause(false)
-    }
-
-    ref?.current?.addEventListener('mouseover', pause)
-    ref?.current?.addEventListener('mouseout', unpause)
-
-    return () => {
-      listener?.removeEventListener('mouseover', pause)
-      listener?.removeEventListener('mouseout', unpause)
-    }
-  }, [ref])
 
   return (
     <div ref={ref} className={['keen-slider', className].join(' ').trim()}>
@@ -80,16 +69,16 @@ const Carousel = ({
           <Arrow
             direction='left'
             className='w-12 h-12 absolute bg-opacity-50 p-1.5 top-1/2 rounded m-1 bg-grey transform -translate-y-1/2 left-0'
-            onClick={() => slider.prev()}
+            onClick={() => slider.current?.prev()}
           />
           <Arrow
             direction='right'
             className='w-12 h-12 absolute bg-opacity-50 top-1/2 p-1.5 rounded m-1 bg-grey transform -translate-y-1/2 right-0'
-            onClick={() => slider.next()}
+            onClick={() => slider.current?.next()}
           />
         </>
       )}
-      {indicators && (
+      {indicators && slider.current && (
         <Dots
           className='absolute bottom-0 left-0 right-0 z-10 flex justify-center'
           nImages={length}
