98de145e9c1b250fed7c314c7eebde8fb4fdbb6b
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2021-12-04 21:40:25 +0800

    Fixed firebase errors. Removed @firebase/app and @firebase/firestore packages and added 'firebase' package again. Imported 'firebase/compat/app' and 'firebase/compat/firestore' which seemed to fix the problem.
    source: https://stackoverflow.com/questions/69139443/property-auth-does-not-exist-on-type-typeof-import-firebase-auth

--- components/Auth/firebase.tsx
+++ components/Auth/firebase.tsx
@@ -1,5 +1,5 @@
-import firebase from '@firebase/app'
-import '@firebase/firestore'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCRl0J6FOUJEOphf3sOiD22yHYDxkJ1QKo',
