95f55c263853308ec7683b048ac72f7bfb1a0ba1
tim <timothy.tan.93@gmail.com>
2021-12-05 12:42:29 +0800

    closes and resets but not centered

--- client/src/Components/Map/index.tsx
+++ client/src/Components/Map/index.tsx
@@ -8,10 +8,8 @@ import svgData from './svgImageData.json'
 import mapImg from './assets/TuckerMap.jpg'
 import descData from './assets/description.json'
 import Townbox from '../Townbox'
-import {TransformWrapper, TransformComponent} from 'react-zoom-pan-pinch'
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 //import internal from 'stream'
-//import Dialogue from 'Components/Dialogue'
-//import bananamanAvatar from 'lib/assets/banana.jpg'
 
 const Map: React.FC = () => {
   // Used because SVG does not scale properly without
@@ -21,7 +19,7 @@ const Map: React.FC = () => {
   const [scale, setScale] = useState(1)
   const [display, setDisplay] = useState(false)
   // const [townbox, setTownbox] = useState(<></>)
-  type HeaderColor = 'primary' | 'orange';
+  type HeaderColor = 'primary' | 'orange'
   useEffect(() => {
     if (elementRef?.current?.clientHeight) {
       setHeight(elementRef?.current?.clientHeight)
@@ -32,28 +30,26 @@ const Map: React.FC = () => {
     function handleResize() {
       const mobileWidth = 2900
       const minStaticWidth = 1170
-      const targetWidth = window.innerWidth > minStaticWidth ? window.innerWidth : mobileWidth
-      setScale(targetWidth/4961)
+      const targetWidth =
+        window.innerWidth > minStaticWidth ? window.innerWidth : mobileWidth
+      setScale(targetWidth / 4961)
     }
-    handleResize();
+    handleResize()
     window.addEventListener('resize', handleResize)
-    },
-    []
-  )
+  }, [])
 
   // to get the area description given an area so you can actually use headers/captions
   // returns null if such area doesn't exist in assets/description.json
   const getAreaDescription = (area: Location) => {
-    for(const description of descData.descriptionArray) {
+    for (const description of descData.descriptionArray) {
       if (description.id === Location[area]) {
         return description
       }
     }
-
     return null
   }
   const close = () => {
-    setSelect(null) 
+    setSelect(null)
     setDisplay(false)
   }
   const onMapClick = (area: Location) => {
@@ -61,114 +57,129 @@ const Map: React.FC = () => {
     setDisplay(!display)
   }
   // eslint-disable-next-line
-  const handleClick = (event: any, setTransform:any, xtrans: number, ytrans: number) => { //need to change this type
+  const handleClick = (
+    event: any,
+    setTransform: any,
+    xtrans: number,
+    ytrans: number
+  ) => {
+    //need to change this type
     event.preventDefault()
     const area = event.target.alt
     onMapClick(Location[area as keyof typeof Location])
-    setTransform(xtrans,ytrans ,2)
+    setTransform(xtrans, ytrans, 2)
   }
-
 
   // Data can be made from dev/svgParse.py
   return (
-  <>
-    <div>
-      <TransformWrapper
-        doubleClick={{ disabled: true }}
-        wheel={{ disabled: true }}
-        initialScale={1}
-      >
-      {({ resetTransform, setTransform }) => (
-                <>
-                <div>
-                  <Townbox close={() => {close; resetTransform()}} />
-                    <button
-                    onClick={() => resetTransform()}
-                  >
-                    Reset
-                  </button>
-                </div>  
-      <TransformComponent>
-      <div
-      ref={elementRef}
-      className='block w-full min-h-full items-stretch'
-      style={{minHeight:'900px'}}
-      >
-      {height === 0 ? null : (
-          <div className='svgrow'>
-            <img src={mapImg} alt="Tucker Island Map" useMap="#tuckerislandmap"/>
+    <>
+      {/* THIS IS THE INSERTION POINT*/}
 
-            <map name="tuckerislandmap">
-              {
-                svgData.groupArray.map(location => {
-                  if (location.coords){
-                   // CHECK SCALING OF ENTIRE IMAGE TO SCREEN
-                   // Seems to need to be scaled because the image map is not the same size as what is actually displayed.
-                   // eg. the image is actually at the top left of the screen and is significantly smaller than what is actually shown
-                   //scaling by 10 seems to give better views of the locations
-                    const xtrans = parseInt(location.xtrans) * scale * 10
-                    const ytrans = parseInt(location.ytrans) * scale * 10
+      <div>
+        <TransformWrapper
+          doubleClick={{ disabled: true }}
+          wheel={{ disabled: true }}
+          initialScale={1}
+        >
+          {({ resetTransform, setTransform }) => (
+            <>
+            <div>                <button onClick={() => resetTransform()}>Reset</button></div>
 
-                    const scaledCoords = location.coords.map(coord => coord*scale)
-                    const className = Location[location.id as keyof typeof Location] === selected
-                          ? 'map-selected'
-                          : 'map-unselected'
-                    return (
-                        <area 
-                          key={location.id}
-                          alt={location.id}
-                          onClick={(e) => { handleClick(e, setTransform, -xtrans, -ytrans)}}
-                          href={location.id}
-                          coords={scaledCoords.join()}
-                          className={className}
-                          shape="poly"
-                        />
-                    )
+
+              <TransformComponent>
+                <div
+                  ref={elementRef}
+                  className='block w-full min-h-full items-stretch'
+                  style={{ minHeight: '900px' }}
+                >
+                  {height === 0 ? null : (
+                    <div className='svgrow'>
+                      <img
+                        src={mapImg}
+                        alt='Tucker Island Map'
+                        useMap='#tuckerislandmap'
+                      />
+
+                      <map name='tuckerislandmap'>
+                        {svgData.groupArray.map(location => {
+                          if (location.coords) {
+                            // CHECK SCALING OF ENTIRE IMAGE TO SCREEN
+                            // Seems to need to be scaled because the image map is not the same size as what is actually displayed.
+                            // eg. the image is actually at the top left of the screen and is significantly smaller than what is actually shown
+                            //scaling by 10 seems to give better views of the locations
+                            const xtrans =
+                              parseInt(location.xtrans) * scale * 10
+                            const ytrans =
+                              parseInt(location.ytrans) * scale * 10
+
+                            const scaledCoords = location.coords.map(
+                              coord => coord * scale
+                            )
+                            const className =
+                              Location[location.id as keyof typeof Location] ===
+                              selected
+                                ? 'map-selected'
+                                : 'map-unselected'
+                            return (
+                              <area
+                                key={location.id}
+                                alt={location.id}
+                                onClick={e => {
+                                  handleClick(e, setTransform, -xtrans, -ytrans)
+                                }}
+                                href={location.id}
+                                coords={scaledCoords.join()}
+                                className={className}
+                                shape='poly'
+                              />
+                            )
+                          }
+                        })}
+                      </map>
+                    </div>
+                  )}
+                </div>
+
+
+                <div className={`full-page-wrapper ${display ? '' : 'none'}`}>
+                {svgData.groupArray.map(area => {
+                  if (selected !== null && area.coords) {
+                    const selectedArea = getAreaDescription(selected)
+                    console.log(selectedArea)
+
+                    if (selectedArea !== null && selectedArea.id === area.id) {
+                      const header = selectedArea?.headerText
+                      const caption = selectedArea?.captionText
+                      const showButton = selectedArea?.showButton
+                      const headerColor = selectedArea?.headerColor as HeaderColor
+
+                      return (
+                        <>
+                          <div
+                            key={selectedArea.id}
+                            className='townbox-wrapper'
+                          >
+                            <Townbox
+                              headerColor={headerColor}
+                              headerText={header}
+                              captionText={caption}
+                              showButton={showButton}
+                              close={() => {
+                                close()
+                                resetTransform()}}
+                            />
+                          </div>
+                        </>
+                      )
+                    }
                   }
-                })
-              }
-            </map>
-        </div>
-      )}
-      
-    </div>
-    </TransformComponent>
-              </>
-              )}
-    </TransformWrapper>
-    </div>
-
-    <div className={`full-page-wrapper ${display ? '': 'none'}`}>
-      {
-        svgData.groupArray.map(area => {
-          if (selected !== null && area.coords){
-            const selectedArea = getAreaDescription(selected)
-            console.log(selectedArea)
-
-            if(selectedArea !== null && selectedArea.id === area.id) {
-              const header = selectedArea?.headerText
-              const caption = selectedArea?.captionText
-              const showButton = selectedArea?.showButton
-              const headerColor = selectedArea?.headerColor as HeaderColor;
-
-              return (
-                <>
-                  <div key={selectedArea.id} className="townbox-wrapper">
-                    <Townbox 
-                      headerColor={headerColor}
-                      headerText={header}
-                      captionText={caption}
-                      showButton = {showButton}
-                      close={close}
-                    />
-                  </div>
-                  </>
-              )
-            }
-          }
-        })
-      }
-    </div>
+                })}
+              </div>
+              </TransformComponent>
+            </>
+          )}
+        </TransformWrapper>
+      </div>
     </>
   )
 }
