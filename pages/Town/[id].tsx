e76cc2fc12eb8e1977ba1fc583a7ab1a2bfd9818
kurapikaaaa <petersheng2@outlook.com>
2021-12-08 15:30:49 +0800

    commit

--- pages/Town/[id].tsx
+++ pages/Town/[id].tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import {useRouter,NextRouter} from 'next/router'
 
 interface Props {
-    
+
 }
 
 const Town = (props: Props) => {
+    const route:NextRouter = useRouter()
+    const {id} = route.query
     return (
         <div>
-            
+            Town a{id} 
         </div>
     )
 }
