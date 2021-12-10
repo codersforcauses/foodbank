179163488c2d49532c004b25592efa7081a73db9
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2021-12-10 21:57:24 +0800

    Made some small next config changes, testing purposes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    optimizeCss: false
-  },
   // need to remove once test is deleted
   images: {
     domains: ['images.unsplash.com', 'tinyurl.com']
