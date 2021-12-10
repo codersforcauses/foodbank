956c46f3f7c5b202f0cefe25c7d7ec1965eb1a3c
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2021-12-10 22:01:11 +0800

    OptimiseCSS in nextconfig set back to true.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    optimizeCss: true
+  },
   // need to remove once test is deleted
   images: {
     domains: ['images.unsplash.com', 'tinyurl.com']
