7dcdf5329141acab72ce53ea3d265a509fb93464
Nicholas Choong <nicholascwc1997@gmail.com>
2021-12-10 21:02:56 +0800

    Moved firebase

---
+++ components/firebase/context.tsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, PropsWithChildren } from 'react'
+import { User, Auth } from 'firebase/auth'
+import { Firestore } from 'firebase/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useDocument } from 'react-firebase-hooks/firestore'
+import { app, auth, db } from '@components/firebase'
+
+interface FirebaseContextProps {
+  auth: Auth
+  db: Firestore
+  user?: User
+  userLoading?: boolean
+  userError?: Error | undefined
+}
+
+const FirebaseContext = createContext<FirebaseContextProps>({
+  auth: auth,
+  db: db
+})
+const useFirebase = () => useContext(FirebaseContext)
+
+const FirebaseProvider = ({ children }: PropsWithChildren<{}>) => {
+  // User Authentication
+  const [user, userLoading, userError] = useAuthState(auth)
+
+  const value: FirebaseContextProps = {
+    auth: auth,
+    db: db,
+    user: user,
+    userLoading: userLoading,
+    userError: userError
+  }
+
+  return (
+    <FirebaseContext.Provider value={value}>
+      {children}
+    </FirebaseContext.Provider>
+  )
+}
+
+export { useFirebase, FirebaseProvider }
