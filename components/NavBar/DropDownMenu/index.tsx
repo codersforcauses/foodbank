6a41cd16790776f431644d62b17dcccf0d36c983
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2021-12-10 16:15:00 +0800

    Changed colour of the menu button and aligned it to the right side. Increased font size of the "Sign-In" option

--- components/NavBar/DropDownMenu/index.tsx
+++ components/NavBar/DropDownMenu/index.tsx
@@ -31,12 +31,19 @@ const DropDownMenu = () => {
     <nav>
       <button
         onClick={() => setSubMenu(!subMenu)}
-        className='px-4 py-1 font-serif text-xl text-white hover:opacity-75 bg-teal rounded-md'
+        className='px-4 py-1 font-serif text-xl text-black hover:opacity-75 bg-teal rounded-md'
       >
         <b>&#9776;</b> MENU
       </button>
       {subMenu && (
-        <div className='shadow-lg overflow-hidden absolute rounded-lg z-10 left-1/2 transform -translate-x-1/2 mt-6 px-2 w-auto max-w-md sm:px-0'>
+        <div
+          className='shadow-lg 
+                        overflow-hidden 
+                        absolute 
+                        rounded-lg 
+                        z-10 
+                        left-1/2 transform -translate-x-1/2 mt-6 px-2 w-auto max-w-md sm:px-0'
+        >
           <div className='relative grid gap-6 bg-primary px-5 py-6 sm:gap-8 sm:p-8'>
             <div className='block items-center'>
               {links.map(nav => (
