cec3bbbab3e64346919bd842da04609382217453
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2021-12-10 21:53:08 +0800

    Disabling optimiseCSS exerimental feature in next config, for testing purposes. (May or may not re-enable it.)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    optimizeCss: true
+    optimizeCss: false
   },
   // need to remove once test is deleted
   images: {
