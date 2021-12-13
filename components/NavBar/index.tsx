581a7adb83eb38fdf7c5633cb528f77468c77bd8
Nicholas Choong <nicholascwc1997@gmail.com>
2021-12-14 03:54:39 +0800

    Testing a different way to display username

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -55,6 +55,7 @@ const Navbar = () => {
         {user ? (
           <DropdownSignOut
             username={capitalize(user!.email!.replace(/@[^@]+$/, ''))}
+            // username={capitalize(user!.displayName)}
             auth={auth}
           />
         ) : (
