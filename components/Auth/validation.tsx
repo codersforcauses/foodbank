90dd6e6d6d2b794c01521e20a698716f720fde48
Nicholas Choong <nicholascwc1997@gmail.com>
2021-12-14 00:25:52 +0800

    Commented out code that is causing error

--- components/Auth/validation.tsx
+++ components/Auth/validation.tsx
@@ -4,13 +4,13 @@ import { LoginFormValues } from './index'
 const validationSchema: Record<LoginFormValues, RegisterOptions> = {
   username: {
     required: 'Please enter your name or a username'
-  },
-  password: {
-    required: 'Please pick your 3 favourite foods'
-  },
-  repeatedPassword: {
-    required: 'Please pick your 3 favourite foods'
   }
+  // ,password: {
+  //   required: 'Please pick your 3 favourite foods'
+  // },
+  // repeatedPassword: {
+  //   required: 'Please pick your 3 favourite foods'
+  // }
 }
 
 export default validationSchema
