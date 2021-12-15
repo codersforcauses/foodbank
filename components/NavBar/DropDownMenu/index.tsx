ba9724e1a7cf8abbb8ffb71ccc2730e09c06df64
Bryce O'Connor <contentaggregator@mail.com>
2021-12-15 10:51:35 +0800

    Added fade-in transition to menu dropdown.

--- components/NavBar/DropDownMenu/index.tsx
+++ components/NavBar/DropDownMenu/index.tsx
@@ -41,12 +41,12 @@ const DropDownMenu = ({ page, route }: DropDownMenuProps) => {
       </button>
       <Transition
         show={subMenu}
-        enter="transition-opacity duration-75"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity duration-150"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
+        enter='transition-opacity duration-75'
+        enterFrom='opacity-0'
+        enterTo='opacity-100'
+        leave='transition-opacity duration-150'
+        leaveFrom='opacity-100'
+        leaveTo='opacity-0'
       >
         <div
           className='shadow-lg 
