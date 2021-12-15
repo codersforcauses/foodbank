f67c97a28d556eae20db1a8056f888bc22ef7fe1
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2021-12-15 22:34:05 +0800

    Reduced dimensions of the logo image

--- components/NavBar/DropDownMenu/index.tsx
+++ components/NavBar/DropDownMenu/index.tsx
@@ -45,9 +45,7 @@ const DropDownMenu = ({ page, route, links }: DropDownMenuProps) => {
           <div className='relative grid gap-6 rounded-lg bg-primary px-5 py-6 sm:gap-8 sm:p-8'>
             <div className='block items-center'>
               {links.map(nav => (
-                <>
-                  <NavLink key={nav.page} {...nav} />
-                </>
+                <NavLink key={nav.page} {...nav} />
               ))}
             </div>
           </div>
