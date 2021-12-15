182600b3efde88d8eae80fbd94b8a9ec08bf2506
HereItGoes <shuangquan44@gmail.com>
2021-12-15 18:22:36 +0800

    sample images and need to work on center issue

--- pages/CharacterPage.tsx
+++ pages/CharacterPage.tsx
@@ -1,23 +1,28 @@
-import bgImg from '/components/Background Assets/Background Assets/BG Blue.jpg'
+import bgImg from '/components/Background Assets/BG Blue.jpg'
 import Image from 'next/image'
-import characterImg from 'components/App 1500 x1500 images/Apple Atomic.png'
+import characterImg from 'components/Background Assets/Apple Atomic.png'
 function CharacterSelection() {
     const bob = 3
     return (
         // <div className="fixed overflow-hidden -z-1">
         <>
             {/* <div className="bg-[url('../components/Background Assets/Background Assets/BG Blue.jpg')]"> */}
-            <Image
-                className='relative -z-10'
-                src={bgImg}
-                alt='background image'
-                layout='fill'
-                objectFit='cover'
-            />
-            <div className='w-2/4 h-2/4'>
-                <Image src={characterImg} alt='character image' />
+
+            <div className='flex flex-row justify-center items-center h-screen'>
+                <Image
+                    className='relative -z-10 opacity-70'
+                    src={bgImg}
+                    alt='background image'
+                    layout='fill'
+                    objectFit='cover'
+                />
+                <div className='w-1/4 h-1/4'>
+                    <Image src={characterImg} alt='character image' />
+                </div>
+                <div className='w-1/4 h-3/4  ml-20 flex flex-row justify-center items-center bg-white z-20 rounded-lg'>
+                    <h1 className=' text-6xl'>Stuff that will be here</h1>
+                </div>
             </div>
-            <h1 className='fixed text-red z-20'>ssss</h1>
             {/* </div> */}
         </>
         // </div>
