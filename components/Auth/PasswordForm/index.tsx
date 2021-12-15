6a5b1b96ddf109fdeab72e1243fdee7ea6c215e6
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2021-12-15 11:11:44 +0800

    Implemented functionality so that the form resets when you press the back button.

--- components/Auth/PasswordForm/index.tsx
+++ components/Auth/PasswordForm/index.tsx
@@ -1,4 +1,4 @@
-import { useState, MouseEventHandler } from 'react'
+import React, { useState, MouseEventHandler, useRef } from 'react'
 import { Button, GridField } from '@components/Custom'
 import { Character } from '@components/Custom/FormComponents/GridField/GridSet'
 import { useForm, FormProvider } from 'react-hook-form'
@@ -43,12 +43,21 @@ const PasswordForm = ({
     return selectedGrid.map(item => item.password).join('')
   }
 
+  const formSelector = useRef<HTMLFormElement>(null)
+
+  const goBack = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (null !== formSelector.current) {
+      formSelector.current.reset()
+    }
+    goPrevPage(e)
+  }
   return (
     <FormProvider {...methods}>
       <form
         onSubmit={methods.handleSubmit(data =>
           updatePassword(getPassword(data.mask))
         )}
+        ref={formSelector}
       >
         <p className='text-lg text-center'>{label}</p>
         {error && <p>{error}</p>}
@@ -60,11 +69,7 @@ const PasswordForm = ({
           updateCount={updateCount}
         />
         <div className='flex justify-center pt-4 space-x-2'>
-          <Button
-            type='button'
-            onClick={goPrevPage}
-            className='flex items-center'
-          >
+          <Button type='button' onClick={goBack} className='flex items-center'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 25 25'
