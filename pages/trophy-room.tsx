45f245266e41f629e1558dea810cb524734b5864
chenga3 <alan.cheng24@gmail.com>
2021-12-15 21:52:09 +0800

    added trophy cabinet, chars to background

--- pages/trophy-room.tsx
+++ pages/trophy-room.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import TrophyCabinet from 'public/images/trophy-cabinet.png'
 
 const TrophyRoom = () => {
   return (
     <div className='bg-primary'>
       <h1 className='text-white font-bold text-6xl text-center'>Trophy Room</h1>
+      <div className='flex items-end'>
+        <div className='melon'>
+          <img src='./images/watermelon.webp' alt='' />
+        </div>
+        <div>
+          <Image src={TrophyCabinet} alt='Cabinet' />
+        </div>
+        <div className='lion'>
+          <img src='./images/lion.webp' alt='' />
+        </div>
+      </div>
     </div>
   )
 }
