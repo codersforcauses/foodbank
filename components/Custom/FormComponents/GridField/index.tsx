612ddbfa6e9047aa9776da6fc5a0d31cec91ce9c
Nicholas Choong <nicholascwc1997@gmail.com>
2021-12-15 16:12:56 +0800

    Moved the defaultMask array to password form

--- components/Custom/FormComponents/GridField/index.tsx
+++ components/Custom/FormComponents/GridField/index.tsx
@@ -8,6 +8,7 @@ const CHARACTERS_FOR_AUTH = 3
 
 export interface GridFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   charSet: Character[]
+  defaultMask: boolean[]
   label: string
   name: string
   rules?: RegisterOptions
@@ -17,6 +18,7 @@ export interface GridFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const GridField = ({
   charSet,
+  defaultMask,
   label,
   rules = {},
   name,
@@ -25,11 +27,8 @@ const GridField = ({
   ...props
 }: GridFieldProps) => {
   const { register, watch, formState, reset } = useFormContext()
-
   const grid = charSet
-  const defaultMask = new Array(9).fill(false)
-
-  const [mask, setMask] = useState(new Array(9).fill(false))
+  const [mask, setMask] = useState(defaultMask)
 
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
@@ -48,57 +47,55 @@ const GridField = ({
   }, [watch, updateCount])
 
   return (
-    <>
-      <div className='grid place-items-center w-full grid-cols-3 gap-2 mb-6 -mt-3'>
-        {grid.map((char, index) => (
-          <div key={char.id} className='relative md:-m-4'>
-            <input
-              type='checkbox'
-              aria-describedby={`${char.name}-label`}
-              //   aria-invalid={!!error}
-              aria-label={`${char.name}-checkbox`}
-              id={char.id}
-              disabled={
-                (!mask[index] && selectedCount === CHARACTERS_FOR_AUTH) ||
-                formState.isSubmitting
-              }
-              className='opacity-0 peer'
-              {...register?.(`mask.${index}`, {
-                ...rules
-              })}
+    <div className='grid place-items-center w-full grid-cols-3 gap-2 mb-6 -mt-3'>
+      {grid.map((char, index) => (
+        <div key={char.id} className='relative md:-m-4'>
+          <input
+            type='checkbox'
+            aria-describedby={`${char.name}-label`}
+            //   aria-invalid={!!error}
+            aria-label={`${char.name}-checkbox`}
+            id={char.id}
+            disabled={
+              (!mask[index] && selectedCount === CHARACTERS_FOR_AUTH) ||
+              formState.isSubmitting
+            }
+            className='opacity-0 peer'
+            {...register?.(`mask.${index}`, {
+              ...rules
+            })}
+          />
+          <label
+            htmlFor={char.id}
+            className='block opacity-100 w-28 xl:w-36 peer-checked:opacity-40'
+          >
+            <Image
+              className='z-0 object-contain transition-all scale-90 md:scale-75 hover:scale-100 '
+              width={250}
+              height={250}
+              layout='responsive'
+              src={char.image}
+              alt={char.name}
             />
-            <label
-              htmlFor={char.id}
-              className='block opacity-100 w-28 xl:w-36 peer-checked:opacity-40'
-            >
-              <Image
-                className='z-0 object-contain transition-all scale-90 md:scale-75 hover:scale-100 '
-                width={250}
-                height={250}
-                layout='responsive'
-                src={char.image}
-                alt={char.name}
-              />
-              <p className='text-sm text-center md:text-lg'>{char.name}</p>
-            </label>
+            <p className='text-sm text-center md:text-lg'>{char.name}</p>
+          </label>
 
-            <BsFillCheckCircleFill
-              className={
-                'absolute top-0 ' +
-                'bottom-0 left-0 ' +
-                'right-0 ' +
-                'text-center ' +
-                'm-auto ' +
-                'text-3xl ' +
-                'z-1 ' +
-                'pointer-events-none ' +
-                'peer-checked:opacity-100 opacity-0'
-              }
-            />
-          </div>
-        ))}
-      </div>
-    </>
+          <BsFillCheckCircleFill
+            className={
+              'absolute top-0 ' +
+              'bottom-0 left-0 ' +
+              'right-0 ' +
+              'text-center ' +
+              'm-auto ' +
+              'text-3xl ' +
+              'z-1 ' +
+              'pointer-events-none ' +
+              'peer-checked:opacity-100 opacity-0'
+            }
+          />
+        </div>
+      ))}
+    </div>
   )
 }
 
