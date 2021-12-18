eeb1586262076a4b006947d427abd009ad2bcfa7
HereItGoes <shuangquan44@gmail.com>
2021-12-18 12:08:14 +0800

    added some css for text box need to work on it more

--- pages/CharacterPage.tsx
+++ pages/CharacterPage.tsx
@@ -8,7 +8,24 @@ function CharacterSelection() {
         <>
             {/* <div className="bg-[url('../components/Background Assets/Background Assets/BG Blue.jpg')]"> */}
 
-            <div className='flex flex-row justify-center items-center h-screen'>
+            <div className='flex items-center justify-center h-screen '>
+                <div className='relative w-1/4 h-1/4 z-20'>
+                    <Image src={characterImg} alt='character image' />
+                </div>
+                <div className='relative w-96 h-max ml-20 flex flex-col justify-center items-center bg-grey-light z-20 rounded-lg font-serif'>
+                    <div className='absolute  -top-10'>
+                        <div className=' h-10 w-40 bottom-10 bg-primary flex justify-center items-center text-white'>Everyday Food</div>
+                        <div className='h-10 w-40 bg-orange flex justify-center items-center text-white'>Character Name </div>
+                    </div>
+                    <div className='p-12'>
+                        <h1 className='text-right'>SUPERPOWERS</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores, possimus reprehenderit optio sequi nostrum quaerat eaque porro ipsa eum iste.</p>
+                        <h1 className='text-right'>USE</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores, possimus reprehenderit optio sequi nostrum quaerat eaque porro ipsa eum iste. Deserunt provident cupiditate quae quaerat, molestiae rerum ipsam officiis sit distinctio consectetur, illum architecto exercitationem, itaque quo dolore. Veniam.</p>
+                        <h1 className='text-right'>LIKES...</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores, possimus reprehenderit optio sequi nostrum quaerat eaque porro ipsa eum iste.</p>
+                    </div>
+                </div>
                 <Image
                     className='relative -z-10 opacity-70'
                     src={bgImg}
@@ -16,12 +33,6 @@ function CharacterSelection() {
                     layout='fill'
                     objectFit='cover'
                 />
-                <div className='w-1/4 h-1/4'>
-                    <Image src={characterImg} alt='character image' />
-                </div>
-                <div className='w-1/4 h-3/4  ml-20 flex flex-row justify-center items-center bg-white z-20 rounded-lg'>
-                    <h1 className=' text-6xl'>Stuff that will be here</h1>
-                </div>
             </div>
             {/* </div> */}
         </>
