73b455f6325679d93dd1427d8aac0011817a67f4
skyheat <skyheatcp@gmail.com>
2021-12-29 18:22:27 +0800

    css for display

--- components/Map/MapImage.tsx
+++ components/Map/MapImage.tsx
@@ -36,48 +36,48 @@ const MapImage = ({
   }
 
   return (
-      <div className='flex relative max-w-screen'>
-        <Image
-          className={styles.mapImage}
-          priority
-          src={baseMap}
-          alt='Tucker Island Map'
-          placeholder='blur'
-          objectPosition='left center'
-          useMap='#tuckerislandmap'
-        />
-        <map name='tuckerislandmap'>
-          {svgData.groupArray.map(location => {
-            if (location.coords) {
-              // CHECK SCALING OF ENTIRE IMAGE TO SCREEN
-              // Seems to need to be scaled because the image map is not the same size as what is actually displayed.
-              // eg. the image is actually at the top left of the screen and is significantly smaller than what is actually shown
-              //scaling by 10 seems to give better views of the locations
-              const xtrans = parseInt(location.xtrans) * scale * 10
-              const ytrans = parseInt(location.ytrans) * scale * 10
+    <div className='flex relative max-w-screen'>
+      <Image
+        className={styles.mapImage}
+        priority
+        src={baseMap}
+        alt='Tucker Island Map'
+        placeholder='blur'
+        objectPosition='left center'
+        useMap='#tuckerislandmap'
+      />
+      <map name='tuckerislandmap'>
+        {svgData.groupArray.map(location => {
+          if (location.coords) {
+            // CHECK SCALING OF ENTIRE IMAGE TO SCREEN
+            // Seems to need to be scaled because the image map is not the same size as what is actually displayed.
+            // eg. the image is actually at the top left of the screen and is significantly smaller than what is actually shown
+            //scaling by 10 seems to give better views of the locations
+            const xtrans = parseInt(location.xtrans) * scale * 10
+            const ytrans = parseInt(location.ytrans) * scale * 10
 
-              const scaledCoords = location.coords.map(coord => coord * scale)
-              //TODO: consider changing className to a state and use tailwind
-              const className =
-                location.id === selected ? 'map-selected' : 'map-unselected'
-              return (
-                <area
-                  key={location.id}
-                  alt={location.id}
-                  onClick={e => {
-                    console.log('test')
-                    handleClick(e, setTransform, -xtrans, -ytrans)
-                  }}
-                  href={location.id}
-                  coords={scaledCoords.join()}
-                  className={className}
-                  shape='poly'
-                />
-              )
-            }
-          })}
-        </map>
-      </div>
+            const scaledCoords = location.coords.map(coord => coord * scale)
+            //TODO: consider changing className to a state and use tailwind
+            const className =
+              location.id === selected ? 'map-selected' : 'map-unselected'
+            return (
+              <area
+                key={location.id}
+                alt={location.id}
+                onClick={e => {
+                  console.log('test')
+                  handleClick(e, setTransform, -xtrans, -ytrans)
+                }}
+                href={location.id}
+                coords={scaledCoords.join()}
+                className={className}
+                shape='poly'
+              />
+            )
+          }
+        })}
+      </map>
+    </div>
   )
 }
 
--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -21,39 +21,39 @@ const Map = () => {
   }, [])
 
   return (
-      <TransformWrapper
-        doubleClick={{ disabled: true }}
-        wheel={{ disabled: true }}
-        initialScale={1}
-      >
-        {({ resetTransform, setTransform }) => (
-          <>
-            <TransformComponent wrapperClass="main">
-              <MapImage
-                scale={scale}
-                setTransform={setTransform}
-                setDisplay={setDisplay}
-                display={display}
-                setSelect={setSelect}
-                selected={select}
-              />
-            </TransformComponent>
+    <TransformWrapper
+      doubleClick={{ disabled: true }}
+      wheel={{ disabled: true }}
+      initialScale={1}
+    >
+      {({ resetTransform, setTransform }) => (
+        <>
+          <TransformComponent wrapperClass='min-h-screen md:main'>
+            <MapImage
+              scale={scale}
+              setTransform={setTransform}
+              setDisplay={setDisplay}
+              display={display}
+              setSelect={setSelect}
+              selected={select}
+            />
+          </TransformComponent>
 
-            <div
-              className={`fixed top-0 left-0 w-screen h-screen flex justify-center ${
-                display ? '' : 'hidden'
-              }`}
-            >
-              <TownBoxWrapper
-                selected={select}
-                resetTransform={resetTransform}
-                setSelect={setSelect}
-                setDisplay={setDisplay}
-              />
-            </div>
-          </>
-        )}
-      </TransformWrapper>
+          <div
+            className={`fixed top-0 left-0 w-screen h-screen flex justify-center ${
+              display ? '' : 'hidden'
+            }`}
+          >
+            <TownBoxWrapper
+              selected={select}
+              resetTransform={resetTransform}
+              setSelect={setSelect}
+              setDisplay={setDisplay}
+            />
+          </div>
+        </>
+      )}
+    </TransformWrapper>
   )
 }
 
