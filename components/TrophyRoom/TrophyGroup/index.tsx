0782a81fd324315559d6631ad5bde388acd4be3e
chenga3 <alan.cheng24@gmail.com>
2022-01-05 19:25:35 +0800

    created TrophyGroup component, overlaid on cabinet, switched to NextJS image

---
+++ components/TrophyRoom/TrophyGroup/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TrophyGroup = () => {
+  return <div>TrophyGroup</div>
+}
+
+export default TrophyGroup
--- pages/trophy-room.tsx
+++ pages/trophy-room.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import Image from 'next/image'
 import TrophyCabinet from 'public/images/trophy-cabinet.png'
+import TrophyGroup from 'components/TrophyRoom/TrophyGroup'
 import { trophies } from 'lib/trophies'
 
 const TrophyRoom = () => {
+  const n = 6
+  console.log(Array(n))
   return (
     <div className='bg-primary'>
       <h1 className='text-white font-bold text-6xl text-center'>Trophy Room</h1>
       <div className='flex items-end'>
         <div className='melon'>
-          <img src='./images/watermelonflipped.webp' alt='' />
+          <Image src='./images/watermelonflipped.webp' alt='' />
         </div>
         <div>
-          <Image src={TrophyCabinet} alt='Cabinet' />
+          <div className='absolute mx-28 my-40 z-10 grid grid-cols-2 gap-y-20 gap-x-36'>
+            {[...Array(n)].map((e, i) => (
+              <TrophyGroup key={i} />
+            ))}
+          </div>
+          <div className='z-0'>
+            <Image src={TrophyCabinet} alt='Cabinet' />
+          </div>
         </div>
         <div className='lion'>
-          <img src='./images/lion.webp' alt='' />
+          <Image src='./images/lion.webp' alt='' />
         </div>
-        {trophies.map(trophy => trophy.name)}
       </div>
     </div>
   )
