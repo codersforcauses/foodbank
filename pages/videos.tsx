49ed6473e861c3fcde7860171aeb9f94fa70087e
j-vanrav <j.vanrav2@gmail.com>
2022-01-08 15:42:05 +0800

    prettify

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -9,29 +9,32 @@ const Player = () => {
     <>
       <button onClick={() => setPopupVisibility(true)}>Open video</button>
       <Transition.Root show={popupVisible} as={Fragment}>
-        <Dialog onClose={() => setPopupVisibility(false)} className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-screen">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-black opacity-30"/>
+        <Dialog
+          onClose={() => setPopupVisibility(false)}
+          className='fixed z-10 inset-0 overflow-y-auto'
+        >
+          <div className='flex items-center justify-center min-h-screen'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0'
+              enterTo='opacity-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100'
+              leaveTo='opacity-0'
+            >
+              <Dialog.Overlay className='fixed inset-0 bg-black opacity-30' />
             </Transition.Child>
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <div className="relative bg-primary rounded max-w-3xl mx-auto p-5">
+              <div className='relative bg-primary rounded max-w-3xl mx-auto p-5'>
                 <PopupVideo url='https://www.youtube.com/watch?v=oUVCWNQFGTc' />
               </div>
             </Transition.Child>
