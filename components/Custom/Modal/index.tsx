063dc0353c559141dce5366a8c3f74823fb11230
APM246 <apm0366@gmail.com>
2022-01-13 00:17:51 +0800

    fixed scrollbar issue and spelling error in Modal component

--- components/Custom/Modal/index.tsx
+++ components/Custom/Modal/index.tsx
@@ -11,10 +11,10 @@ const Modal = ({
   let baseClass = 'fixed z-40 w-full h-full overflow-y-auto origin-center transform -translate-x-1/2 -translate-y-1/2 md:w-3/4 md:h-5/6 inset-1/2'
   let stickyClass = 'sticky inset-x-0 top-0 z-50 flex items-center justify-center px-4 py-2 space-x-4 text-white bg-primary rounded-t-xl'
   if (size === 'sm') sizeClass = 'max-w-lg'
-  if (size === 'md') sizeClass = 'max-w-2lg'
+  if (size === 'md') sizeClass = 'max-w-2xl'
   if (size === 'xl') {
-    sizeClass = "max-w-max";
-    baseClass = 'fixed z-40 w-full h-full overflow-y-auto origin-center transform -translate-x-1/2 -translate-y-3/4 md:w-3/4 lg:w-3/4 inset-1/2 mt-20';
+    sizeClass = 'max-w-max'
+    baseClass = 'fixed z-40 w-full h-full overflow-y-auto origin-center transform -translate-x-1/2 -translate-y-1/2 md:w-3/4 lg:w-3/4 inset-1/2 mt-20';
     stickyClass = 'inset-x-0 top-0 z-50 flex items-center justify-center px-4 py-2 space-x-4 text-white bg-primary rounded-t-xl';
   }
   return (
@@ -38,14 +38,9 @@ const Modal = ({
           leave='transition-all ease-in duration-150'
           leaveFrom='opacity-100 scale-100'
           leaveTo='opacity-0 scale-95'
-          className={[
-            baseClass,
-            sizeClass
-          ]
-            .join(' ')
-            .trim()}
+          className={[baseClass, sizeClass].join(' ').trim()}
         >
-          <div className='relative z-50 transform translate-y-1/2 bg-white rounded-xl'>
+          <div className='relative z-50 transform bg-white rounded-xl'>
             <div className={stickyClass}>
               <Dialog.Title className='font-serif text-2xl'>
                 {heading}
--- pages/recipes/[slug]/slideshow.tsx
+++ pages/recipes/[slug]/slideshow.tsx
@@ -1,51 +1,49 @@
-import { Carousel } from "@components/Custom";
-import styles from "./Slideshow.module.css";
-import Image from "next/image";
-import { Recipe } from 'lib/types';
+import { Carousel } from '@components/Custom'
+import styles from './Slideshow.module.css'
+import Image from 'next/image'
+import { Recipe } from 'lib/types'
 
 export interface RecipeProps {
   recipe: Recipe
 }
 
-const RecipeSlideShow: React.FC<RecipeProps> = ( { recipe } ) => {
-
-  if (!recipe) return <div> Error </div>;
+const RecipeSlideShow: React.FC<RecipeProps> = ({ recipe }) => {
+  if (!recipe) return <div> Error </div>
 
   return (
-    <div
-      className="flex justify-center items-center"
-      // style={{ height: 'calc(100vh - 4rem)' }}
-    >
+    <div className='flex justify-center items-center'>
       <Carousel
         controls
         indicators
         length={recipe.steps.length}
-        className="h-full"
+        className='h-full'
       >
         {recipe.steps.map((recipeStep, index) => (
           // make sure to declare a div as below with `keen-slider__slide` as a class for it to work properly
           <div
             key={`recipeStep_${index}`}
-            className="keen-slider__slide flex flex-col md:flex-row justify-center flex-wrap h-full min-w-32 py-8"
+            className='keen-slider__slide flex flex-col md:flex-row justify-center flex-wrap h-full min-w-32 py-8'
           >
             <div className='w-full lg:w-1/2 flex justify-center'>
               <div className='m-auto py-5'>
                 <div className='w-max h-max px-6 py-2 transform -rotate-12 text-white text-5xl bg-primary font-sans font-bold border-8 border-black'>{`STEP ${
                   index + 1
                 }`}</div>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <div className={styles["card-image-container"] + " flex justify-center"}>
+                <div
+                  className={
+                    styles['card-image-container'] + ' flex justify-center'
+                  }
+                >
                   <Image
                     src={recipeStep.image}
                     alt={`step ${index}`}
-                    className={styles.image + " border-8"}
-                    layout="fill"
+                    className={styles.image + ' border-8'}
+                    layout='fill'
                   />
                 </div>
               </div>
             </div>
-            <div
-              className="font-sans py-5 md:py-0 font-bold text-3xl px-6 w-full lg:w-5/12 min-w-32 flex justify-center items-center">
+            <div className='font-sans py-5 md:py-0 font-bold text-3xl px-6 w-full lg:w-5/12 min-w-32 flex justify-center items-center'>
               {recipeStep.description}
             </div>
           </div>
