3cfd8112f04991fa7d931bca36139a83096bb4a8
tim <timothy.tan.93@gmail.com>
2022-01-19 13:07:18 +0800

    use switch statement

--- components/Custom/Modal/index.tsx
+++ components/Custom/Modal/index.tsx
@@ -8,19 +8,28 @@ const Modal = ({
   size,
   ...props
 }: PropsWithChildren<ModalProps>) => {
-  let sizeClass = 'max-w-4xl'
-  let baseClass =
-    'fixed z-40 w-full h-screen overflow-y-auto origin-center transform -translate-x-1/2 -translate-y-1/2 md:w-3/4 md:h-5/6 inset-1/2 rounded-t-xl'
-  let stickyClass =
+  let sizeClass, baseClass, stickyClass =
     'sticky inset-x-0 top-0 z-50 flex items-center justify-center px-4 py-2 space-x-4 text-white bg-primary rounded-t-xl'
 
-  if (size === 'sm') sizeClass = 'max-w-lg'
-  if (size === 'md') sizeClass = 'max-w-2xl'
-  if (size === 'xl' || size === '2xl') {
-    sizeClass = 'max-w-max'
-    baseClass =
-      'fixed z-40 w-full h-screen overflow-y-auto origin-center transform -translate-x-1/2 -translate-y-1/2 md:w-3/4 lg:w-3/4 inset-1/2 mt-20'
+  switch (size) {
+    case 'sm':
+      sizeClass = 'max-w-lg'
+      break
+    case 'md':
+      sizeClass = 'max-w-2xl'
+      break
+    case 'xl' || '2xl':
+      sizeClass = 'max-w-max'
+      baseClass =
+        'fixed z-40 w-full h-screen overflow-y-auto origin-center transform -translate-x-1/2 -translate-y-1/2 md:w-3/4 lg:w-3/4 inset-1/2 mt-20'
+      break
+    default:
+      sizeClass = 'max-w-4xl'
+      baseClass =
+    'fixed z-40 w-full h-screen overflow-y-auto origin-center transform -translate-x-1/2 -translate-y-1/2 md:w-3/4 md:h-5/6 inset-1/2 rounded-t-xl'
   }
+
+  let finalClass = [baseClass, sizeClass].join(' ').trim()
 
   return (
     <Transition show={props.open} as={Fragment}>
@@ -43,7 +52,7 @@ const Modal = ({
           leave='transition-all ease-in duration-150'
           leaveFrom='opacity-100 scale-100'
           leaveTo='opacity-0 scale-95'
-          className={[baseClass, sizeClass].join(' ').trim()}
+          className={finalClass}
         >
           <div className='relative z-50 transform bg-white rounded-xl'>
             <div className={stickyClass}>
