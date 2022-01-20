1e7068f883d6fa677369b77736f7b4e666389f89
Nicholas Choong <nicholascwc1997@gmail.com>
2022-01-20 19:55:22 +0800

    updated for v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     colors: {
       transparent: 'transparent',
@@ -25,9 +26,6 @@ module.exports = {
         serif: ['Abraham']
       }
     }
-  },
-  variants: {
-    extend: {}
   },
   plugins: []
 }
