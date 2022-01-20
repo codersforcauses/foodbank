5f92eef2b786881101a2c0ce9ca0173f42da3f58
Nicholas Choong <nicholascwc1997@gmail.com>
2022-01-20 12:53:00 +0800

    added mroe guard statements

--- components/API/getData.ts
+++ components/API/getData.ts
@@ -12,16 +12,17 @@ const getAllRecipes = async () => {
 
   // pre filter the original data, making sure there's no entries with empty properties
   const filtered_data = data.results.filter(result => {
-    const CategoryProp = result?.properties?.Category
-    const RecipeProp = result?.properties?.Recipe
-    const EquipmentProp = result?.properties?.Equipment
-    const characterIdProp = result?.properties?.characterId
-    const colorSchemeProp = result?.properties?.colorScheme
-    const equipmentImgProp = result?.properties?.equipmentImg
-    const finalShotProp = result?.properties?.finalShot
-    const ingredientsProp = result?.properties?.ingredients
-    const ingredientsImgProp = result?.properties?.ingredientsImg
-    const slugProp = result?.properties?.slug
+    if (!('properties' in result)) return
+    const CategoryProp = result.properties?.Category
+    const RecipeProp = result.properties?.Recipe
+    const EquipmentProp = result.properties?.Equipment
+    const characterIdProp = result.properties?.characterId
+    const colorSchemeProp = result.properties?.colorScheme
+    const equipmentImgProp = result.properties?.equipmentImg
+    const finalShotProp = result.properties?.finalShot
+    const ingredientsProp = result.properties?.ingredients
+    const ingredientsImgProp = result.properties?.ingredientsImg
+    const slugProp = result.properties?.slug
     return (
       (CategoryProp?.type === 'multi_select'
         ? CategoryProp.multi_select[0]
@@ -61,15 +62,16 @@ const getAllRecipes = async () => {
 
   const getCharacterProps = (charId: String) => {
     const character = chars.results.filter(char => char.id == charId)
-    const nameProp = character[0]?.properties?.name
-    const aliasNameProp = character[0]?.properties?.aliasName
-    const aboutProp = character[0]?.properties?.About
-    const aliasImageProp = character[0]?.properties?.aliasImage
-    const imageGifProp = character[0]?.properties?.imageGif
-    const superPowersProp = character[0]?.properties?.superPowers
-    const foodGroupProp = character[0]?.properties?.foodGroup
-    const locationProp = character[0]?.properties?.location
-    const facingProp = character[0]?.properties?.facing
+    if (!('properties' in character[0])) return {}
+    const nameProp = character[0].properties?.name
+    const aliasNameProp = character[0].properties?.aliasName
+    const aboutProp = character[0].properties?.About
+    const aliasImageProp = character[0].properties?.aliasImage
+    const imageGifProp = character[0].properties?.imageGif
+    const superPowersProp = character[0].properties?.superPowers
+    const foodGroupProp = character[0].properties?.foodGroup
+    const locationProp = character[0].properties?.location
+    const facingProp = character[0].properties?.facing
     return {
       name: nameProp?.type === 'title' ? nameProp.title[0].plain_text : '',
       aliasName:
@@ -103,7 +105,7 @@ const getAllRecipes = async () => {
       location:
         locationProp?.type === 'multi_select'
           ? locationProp.multi_select.map(item => item.name)
-          : [],
+          : '',
       facing:
         facingProp?.type === 'rich_text'
           ? facingProp.rich_text[0].plain_text
@@ -112,6 +114,7 @@ const getAllRecipes = async () => {
   }
 
   const recipes = filtered_data.map(recipe => {
+    if (!('properties' in recipe)) return {}
     const nameProp = recipe.properties.Recipe
     const categoryProp = recipe.properties.Category
     const tagsProp = recipe.properties.Tags
@@ -209,9 +212,10 @@ const getRecipeDetails = async (slug: string) => {
   let steps: RecipeStep[] = []
 
   steps_db.results.map(step => {
-    const numberProp = step?.properties?.number
-    const imageProp = step?.properties?.image
-    const descriptionProp = step?.properties?.description
+    if (!('properties' in step)) return {}
+    const numberProp = step.properties?.number
+    const imageProp = step.properties?.image
+    const descriptionProp = step.properties?.description
     const data = {
       number:
         numberProp?.type === 'title'
@@ -235,22 +239,23 @@ const getRecipeDetails = async (slug: string) => {
     return a.number - b.number
   })
 
-  const characterIdProp = recipe_data.results[0]?.properties?.characterId
+  if (!('properties' in recipe_data.results[0])) return {}
+  const characterIdProp = recipe_data.results[0].properties?.characterId
   const characterId =
     characterIdProp?.type === 'relation' ? characterIdProp.relation[0].id : ''
   const character_data = await notion.pages.retrieve({
     page_id: characterId
   })
-
-  const characterNameProp = character_data?.properties?.name
-  const aliasNameProp = character_data?.properties?.aliasName
-  const aboutProp = character_data?.properties?.About
-  const aliasImageProp = character_data?.properties?.aliasImage
-  const imageGifProp = character_data?.properties?.imageGif
-  const superPowersProp = character_data?.properties?.superPowers
-  const foodGroupProp = character_data?.properties?.foodGroup
-  const locationProp = character_data?.properties?.location
-  const facingProp = character_data?.properties?.facing
+  if (!('properties' in character_data)) return {}
+  const characterNameProp = character_data.properties?.name
+  const aliasNameProp = character_data.properties?.aliasName
+  const aboutProp = character_data.properties?.About
+  const aliasImageProp = character_data.properties?.aliasImage
+  const imageGifProp = character_data.properties?.imageGif
+  const superPowersProp = character_data.properties?.superPowers
+  const foodGroupProp = character_data.properties?.foodGroup
+  const locationProp = character_data.properties?.location
+  const facingProp = character_data.properties?.facing
 
   const character = {
     name:
@@ -286,23 +291,23 @@ const getRecipeDetails = async (slug: string) => {
     location:
       locationProp?.type === 'multi_select'
         ? locationProp.multi_select.map(item => item.name)
-        : [],
+        : '',
     facing:
       facingProp?.type === 'rich_text' ? facingProp.rich_text[0].plain_text : ''
   }
 
   // recipe_data.character = character
-  const recipeNameProp = recipe_data.results[0]?.properties?.Recipe
-  const categoryProp = recipe_data.results[0]?.properties?.Category
-  const tagsProp = recipe_data.results[0]?.properties?.Tags
-  const equipmentProp = recipe_data.results[0]?.properties?.Equipment
-  const ingredientsProp = recipe_data.results[0]?.properties?.ingredients
-  const equipmentImgProp = recipe_data.results[0]?.properties?.equipmentImg
-  const ingredientsImgProp = recipe_data.results[0]?.properties?.ingredientsImg
-  const finalShotProp = recipe_data.results[0]?.properties?.finalShot
-  const colorSchemeNameProp = recipe_data.results[0]?.properties?.colorScheme
-  const hintProp = recipe_data.results[0]?.properties?.hint
-  const slugProp = recipe_data.results[0]?.properties?.slug
+  const recipeNameProp = recipe_data.results[0].properties?.Recipe
+  const categoryProp = recipe_data.results[0].properties?.Category
+  const tagsProp = recipe_data.results[0].properties?.Tags
+  const equipmentProp = recipe_data.results[0].properties?.Equipment
+  const ingredientsProp = recipe_data.results[0].properties?.ingredients
+  const equipmentImgProp = recipe_data.results[0].properties?.equipmentImg
+  const ingredientsImgProp = recipe_data.results[0].properties?.ingredientsImg
+  const finalShotProp = recipe_data.results[0].properties?.finalShot
+  const colorSchemeNameProp = recipe_data.results[0].properties?.colorScheme
+  const hintProp = recipe_data.results[0].properties?.hint
+  const slugProp = recipe_data.results[0].properties?.slug
 
   const recipe: Recipe = {
     page_id: recipe_data.results[0]?.id,
