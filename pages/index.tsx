e9b9c1b4a49fa8a98a3ee4c5434a3e441868a76a
Nicholas Choong <nicholascwc1997@gmail.com>
2022-01-21 15:27:42 +0800

    testing firestore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Map from '@components/Map'
+import TestData from '@components/TestData'
 
 const Home = () => {
-  return <Map />
+  //   return <Map />
+  return <TestData />
 }
 export default Home
