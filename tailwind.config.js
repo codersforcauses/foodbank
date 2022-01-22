bcf7feb8a385af7ac7af4c773ec9f67042c3ca74
Nicholas Choong <nicholascwc1997@gmail.com>
2022-01-22 15:22:50 +0800

    cleaned config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  media: false, // or 'media' or 'class'
   theme: {
     colors: {
       transparent: 'transparent',
