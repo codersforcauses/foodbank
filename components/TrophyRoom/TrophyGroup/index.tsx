1868c89818256ac37fc2148c4263a39f9b7dba42
chenga3 <alan.cheng24@gmail.com>
2022-01-22 12:29:04 +0800

    slightly better positioning

--- components/TrophyRoom/TrophyGroup/index.tsx
+++ components/TrophyRoom/TrophyGroup/index.tsx
@@ -14,13 +14,13 @@ const TrophyGroup = ({ trophy1, trophy2, trophy3, side }: Props) => {
   return (
     <div
       className={
-        '' +
+        'pt-3 sm:h-11 md:h-12 lg:h-24 xl:h-28' +
         (side === 'left'
           ? 'sm:pl-9 md:pl-12 lg:pl-14 xl:pl-16'
           : 'sm:pr-9 md:pl-2 md:pr-10 lg:pl-2 lg:pr-12 xl:pl-2 xl:pr-14')
       }
     >
-      <div className='flex items-center sm:h-11 md:h-12 lg:h-20 xl:h-24'>
+      <div className='flex items-center'>
         {trophy1 && (
           <div className='w-1/3'>
             <Image src={trophy1} alt='Trophy' />
@@ -37,7 +37,7 @@ const TrophyGroup = ({ trophy1, trophy2, trophy3, side }: Props) => {
           </div>
         )}
       </div>
-      <div className='relative w-full bg-white h-5'>
+      <div className='relative w-full bg-white h-5 bottom-0'>
         <div className='absolute w-full font-serif text-center text-sm'>
           {numTrophies} / 3
         </div>
