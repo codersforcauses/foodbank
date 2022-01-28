86ad2aaa7ab86ff14e43b4b0fdd1d8c786ad9db5
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2022-01-28 21:56:39 +0800

    - Added react-hook-form library since it was missing - Got rid of custom breakpoints to avoid inconsistencies from standard frontend web development - Fixed css problems - Fixed responsiveness issues
    * I think this branch is complete and ready to be merged once and for all :) *

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,18 +25,9 @@ module.exports = {
         sans: ['Bliss'],
         serif: ['Abraham']
       }
-    },
-    screens: {
-      xsm: '321px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1281px',
-      '2xl': '1536px'
     }
   },
   variants: {
     extend: {}
-  },
-  plugins: [require('tailwind-scrollbar-hide')]
+  }
 }
