4619f324b7094292476453fc91fdc676645796f9
Nicholas Choong <nicholascwc1997@gmail.com>
2022-01-29 15:00:18 +0800

    declared funtions before html

--- components/TestData/index.tsx
+++ components/TestData/index.tsx
@@ -3,34 +3,35 @@ import { useFirebase } from '@components/FirebaseContext'
 
 const TestData = () => {
   const { achievementsCount, addAchievementsCount, userLoading } = useFirebase()
+
+  const decreaseCount = () => {
+    addAchievementsCount?.(-1)
+  }
+
+  const increaseCount = () => {
+    addAchievementsCount?.(1)
+  }
+
   return (
     <div>
       {userLoading ? (
-        <>
-          <div className='flex items-center justify-center'>
-            <div
-              className='inline-block w-8 h-8 border-4 rounded-full spinner-border animate-spin text-primary'
-              role='status'
-            />
-            Loading...
-          </div>
-        </>
+        <div className='flex items-center justify-center'>
+          <div
+            className='inline-block w-8 h-8 border-4 rounded-full spinner-border animate-spin text-primary'
+            role='status'
+          />
+          Loading...
+        </div>
       ) : (
         <div className='flex justify-center flex-col'>
           <p className='text-center text-2xl'>
             Achievements Count: {achievementsCount.count}
           </p>
           <div className='flex justify-center gap-6'>
-            <Button
-              className='content-center text-2xl'
-              onClick={() => addAchievementsCount?.(-1)}
-            >
+            <Button className='text-2xl' onClick={decreaseCount}>
               -
             </Button>
-            <Button
-              className='text-2xl'
-              onClick={() => addAchievementsCount?.(1)}
-            >
+            <Button className='text-2xl' onClick={increaseCount}>
               +
             </Button>
           </div>
