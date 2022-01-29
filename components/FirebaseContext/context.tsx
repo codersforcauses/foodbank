27b28fd9ab79a5e3b31b21085a7b704c19405828
Nicholas Choong <nicholascwc1997@gmail.com>
2022-01-29 19:54:14 +0800

    moved firebase context to here

---
+++ components/FirebaseContext/context.tsx
@@ -0,0 +1,33 @@
+import { createContext, SetStateAction, useContext } from 'react'
+import { User, Auth } from 'firebase/auth'
+import { Firestore } from 'firebase/firestore'
+import { auth, db } from 'pages/api/firebase'
+
+export interface FirebaseContextProps {
+  auth: Auth
+  db: Firestore
+  user?: User | null
+  userLoading?: boolean
+  userError?: Error
+  achievementsCount: AchievementsCountProp
+  addAchievementsCount?: (newAchievementsEarned: number) => void
+  signOutClearDataUnlockGrid?: () => void
+  gridDisabled?: boolean
+  setGridDisabled?: (value: SetStateAction<boolean>) => void
+}
+
+export interface AchievementsCountProp {
+  count: number
+}
+
+export const defaultAchievementsCount: AchievementsCountProp = { count: 0 }
+
+const FirebaseContext = createContext<FirebaseContextProps>({
+  auth: auth,
+  db: db,
+  achievementsCount: defaultAchievementsCount
+})
+
+export const FirebaseContextProvider = FirebaseContext.Provider
+
+export const useFirebase = () => useContext(FirebaseContext)
