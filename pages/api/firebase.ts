e2b204fb002f597f23555df3b84a3a19f3320d0d
Nicholas Choong <nicholascwc1997@gmail.com>
2022-01-29 16:24:13 +0800

    moved api keys to .env but keys still expose as
    frontend needs know them inorder to interact with Firebase

--- pages/api/firebase.ts
+++ pages/api/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCRl0J6FOUJEOphf3sOiD22yHYDxkJ1QKo',
-  authDomain: 'foodbank-c9a2f.firebaseapp.com',
-  projectId: 'foodbank-c9a2f',
-  storageBucket: 'foodbank-c9a2f.appspot.com',
-  messagingSenderId: '555238684160',
-  appId: '1:555238684160:web:f8a2b5d94fc398d7564ae5'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
 export const app = initializeApp(firebaseConfig)
