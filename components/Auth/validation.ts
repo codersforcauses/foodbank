fe93c5030d175a0e3796a66ae91b6f44e86a3668
Nicholas Choong <nicholascwc1997@gmail.com>
2022-02-05 04:56:36 +0800

    added validation schema for react hook form

---
+++ components/Auth/validation.ts
@@ -0,0 +1,20 @@
+import { RegisterOptions } from 'react-hook-form'
+
+import { LoginFormValues } from './index'
+
+const validationSchema: Record<LoginFormValues, RegisterOptions> = {
+  username: {
+    required: 'Please enter your name or a username',
+    pattern: {
+      value: /^[a-z0-9]+$/i,
+      message: 'Alphanumeric characters only'
+    },
+    minLength: {
+      value: 3,
+      message: 'Choose your three characters'
+    }
+    // onChange: handleUsernameChange
+  }
+}
+
+export default validationSchema
