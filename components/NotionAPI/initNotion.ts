f4029b191fbdb8b7be4189976e3c25f2015a0a03
Nicholas Choong <nicholascwc1997@gmail.com>
2022-02-19 15:15:11 +0800

    added guard clauses for reading in env var

--- components/NotionAPI/initNotion.ts
+++ components/NotionAPI/initNotion.ts
@@ -1,5 +1,16 @@
 import { Client } from '@notionhq/client'
 
+if (process.env.NOTION_API_KEY === undefined) {
+  console.error('[ FATAL ]: NO NOTION_API_KEY IN ENVIRONMENT VARIABLES')
+  process.exit(1)
+}
+if (process.env.NOTION_CHARACTERS_DB_ID === undefined) {
+  console.error(
+    '[ FATAL ]: NO NOTION_CHARACTERS_DB_ID IN ENVIRONMENT VARIABLES'
+  )
+  process.exit(1)
+}
+
 const notion = new Client({
   auth: process.env.NOTION_API_KEY
 })
