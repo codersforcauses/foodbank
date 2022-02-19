11a8aed31be733f60ff102b8325b32bfc8e88b83
skyheat <skyheatcp@gmail.com>
2022-02-19 14:38:49 +0800

    next-pwa setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
+// next.confg.js
+
+const withPWA  = require("next-pwa");
+module.exports = withPWA({
+ //...before
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+  //...after
+});
+
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="A web application aiming to educate young children about healthy eating habits and food groups." />
+        <meta name="theme-color" content="#671E75" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
