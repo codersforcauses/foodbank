a7f83566d469b454ca5cc1787ff5010a36766784
Alan Cheng <alan.cheng24@gmail.com>
2022-02-19 14:08:59 +0800

    removed test code

--- pages/trophy-room.tsx
+++ pages/trophy-room.tsx
@@ -1,14 +1,11 @@
-import React, { useState } from 'react'
 import Image from 'next/image'
 
-import TestData from '@components/TestData'
 import TrophyCabinet from '@components/TrophyRoom/TrophyCabinet'
 
 import VitaminCLion from 'public/images/lion.webp'
 import Melonator from 'public/images/watermelonflipped.webp'
 
 const TrophyRoom = () => {
-  const [numUnlocked, setNumUnlocked] = useState(12)
   return (
     <div className='bg-primary min-h-screen md:pt-14'>
       <h1 className='text-white font-bold text-6xl text-center pt-10'>
@@ -22,28 +19,6 @@ const TrophyRoom = () => {
         <div className='max-w-sm hidden sm:block'>
           <Image src={VitaminCLion} alt='Vitamin C Lion' />
         </div>
-      </div>
-
-      {/* TEMPORARY CONTAINER - FOR TESTING PURPOSES, TO INCREASE AND DECREASE TROPHIES IN CABINET. Remove once firestore is integrated */}
-      <div className='flex justify-center'>
-        <button
-          onClick={() => {
-            if (numUnlocked == 18) return
-            setNumUnlocked(prev => prev + 1)
-          }}
-          className='bg-green rounded-lg text-4xl w-10 m-2'
-        >
-          +
-        </button>
-        <button
-          onClick={() => {
-            if (numUnlocked == 0) return
-            setNumUnlocked(prev => prev - 1)
-          }}
-          className='bg-red rounded-lg text-4xl w-10 m-2'
-        >
-          -
-        </button>
       </div>
     </div>
   )
