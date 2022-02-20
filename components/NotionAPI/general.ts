f39b98174aa5de7629a870fc49c8b7366ffbd570
Alan Cheng <alan.cheng24@gmail.com>
2022-02-20 11:53:27 +0800

    some jsdoc comments

--- components/NotionAPI/general.ts
+++ components/NotionAPI/general.ts
@@ -1,6 +1,11 @@
 // This file contains useful functions for creating custom API functions that leverages Notion API.
 
-/** Returns a column value of a Notion DB item/row identified by the column name. Title of the item/row is needed for meaningful error messages (make sure to call `getTitle` before any calls to this function).*/
+/** Returns a column value of a Notion DB item/row identified by the column name.
+ * Title of the item/row is needed for meaningful error messages (make sure to call `getTitle` before any calls to this function).
+ * @param result - A result from a Notion DB query response.
+ * @param colName - Name of the column to retrieve a value from.
+ * @param title - Title of the database item to retrieve a value from.
+ */
 const getColVal = (result: any, colName: string, title: string): string => {
   if (!(colName in result.properties)) {
     throw new Error('Could not find property "' + colName + '".')
@@ -46,9 +51,12 @@ const getColVal = (result: any, colName: string, title: string): string => {
   }
 }
 
-/** Returns the title of a Notion DB item/row. */
-const getTitle = (result: any, titlecolName: string): string => {
-  const titleProp = result.properties[titlecolName]
+/** Returns the title of a Notion DB item/row.
+ * @param result - A result from a Notion DB query response.
+ * @param titleColName - The name of the title column.
+ */
+const getTitle = (result: any, titleColName: string): string => {
+  const titleProp = result.properties[titleColName]
   if (titleProp.type !== 'title') throw new Error('Missing title.')
   return titleProp.title[0].plain_text
 }
