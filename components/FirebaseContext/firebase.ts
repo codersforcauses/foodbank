d9506c73c9ad9df677d54c0c44f09d45553fd97d
Nicholas Choong <62389033+NicholasChoong@users.noreply.github.com>
2022-10-02 22:26:53 +0800

    added analytics (#276)

--- components/FirebaseContext/firebase.ts
+++ components/FirebaseContext/firebase.ts
@@ -1,3 +1,4 @@
+import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
@@ -8,9 +9,16 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+
+let analytics
+if (firebaseConfig.measurementId) {
+  analytics = getAnalytics(app)
+}
+export { analytics }
