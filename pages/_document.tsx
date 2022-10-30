badf65e98e44b963961ed3a5c6edb3b4a47cc556
superhero-foods <105845767+superhero-foods@users.noreply.github.com>
2022-10-30 14:46:50 +0800

    Fixing my mistake

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VHXVXQRJEE"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VHXVXQRJEE');
+        </script>
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta
@@ -15,15 +24,6 @@ export default function Document() {
         <link rel='shortcut icon' href='/favicon.ico' />
         <link rel='apple-touch-icon' href='icons/apple-touch-icon.png'></link>
       </Head>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-VHXVXQRJEE"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-VHXVXQRJEE');
-      </script>
       <body>
         <Main />
         <NextScript />
